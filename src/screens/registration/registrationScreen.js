15119eb0261e3c66f384b9a590bf0c0e586ffa70
Joey Corcoran <81182125+joeycor92@users.noreply.github.com>
2022-10-03 18:26:26 +0100

    Created new branch for Registration

---
+++ src/screens/registration/registrationScreen.js
@@ -0,0 +1,6 @@
+/// Registration Screen ///
+// Route:
+//  <URL>/register
+// Description:
+//  This screen contains the components rendered to the user when they are registering to use the site.
+

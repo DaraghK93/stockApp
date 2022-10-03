f173fb5f1931caac505f18509c3d79a6fe707cb7
Joey Corcoran <81182125+joeycor92@users.noreply.github.com>
2022-10-03 12:28:48 +0100

    Updates made as per Warren's feedback. Removal of the main body of the app and unused imports

--- src/App.js
+++ src/App.js
@@ -4,45 +4,11 @@ import {BrowserRouter as Router,Routes,Route,Navigate } from 'react-router-dom'
 import {useState} from 'react';
 
 
-const apiName = 'stockapi'
-const myInit = { // OPTIONAL
-    headers: {}, // OPTIONAL
-    response: true, // OPTIONAL (return the entire Axios response object instead of only response.data)
-    queryStringParameters: {  // OPTIONAL
-        name: 'param',
-    },
-};
-
 function App() {
-  const [stocks, setStocks] = useState([])
-
-
-  function getStocks(e){
-    e.preventDefault();
-    API
-      .get(apiName,'/stock', myInit)
-      .then(response => {
-        console.log(response)
-        setStocks(response.data)
-      })
-      .catch(error => {
-        console.log(error)
-      })
-  }
-
   return (
-    <div className="App">
-      <h1>Stock Application</h1> 
-      <button onClick={getStocks}>
-        Make a GET request to stocks
-      </button>
-      <ul>
-        {stocks.map((stock) => (
-          <li key={stock._id}>{stock.name} - {stock.ticker} - {stock.value} - {stock.createdAt}</li>
-        ))}
-      </ul>
-    </div>
+    <Router> 
+      
+    </Router>
   );
 }
-
 export default App;

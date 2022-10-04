ccf64cae336524a69401a15ce1f6d264373fb02d
Joey Corcoran <81182125+joeycor92@users.noreply.github.com>
2022-10-04 12:11:54 +0100

    Update following the creation of redux folder

--- src/screens/login/loginscreen.js
+++ src/screens/login/loginscreen.js
@@ -3,40 +3,41 @@
 //  <URL>/login
 // Description:
 //  This screen contains the components redenred to the user when they are logging in
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import {Form, Button, Row, Col, Container} from 'react-bootstrap'
-// import { useDispatch, useSelector } from 'react-redux'
-// import { login } from '../actions/userActions'
-import { useNavigate,Link } from "react-router-dom"
+import { useDispatch, useSelector } from 'react-redux'
+import { login } from '../../actions/userActions'
+import { useNavigate } from "react-router-dom"
 
 function LoginPage() {
     const [email, setEmail] = useState('')
 	const [password, setPassword] = useState('')
 
-    // const navigate = useNavigate()
-    // const dispatch = useDispatch()
-	// const user = useSelector((state) => state.user)
-    // const {loading, error, userInfo} = user;
-    // function handleSubmit(event) {
-    //     event.preventDefault();
-    //     dispatch(login(email,password));
-    //   }
+    const navigate = useNavigate()
+    const dispatch = useDispatch()
+	const user = useSelector((state) => state.user)
+    const {userInfo} = user;
+    function handleSubmit(event) {
+        event.preventDefault();
+        dispatch(login(email,password));
+      }
        
 
-    //   useEffect (() => {
-    //     if(userInfo){
+      useEffect (() => {
+        if(userInfo){
 
-    //       navigate('/userDashboard');
-    //     }
-    //   },[userInfo,navigate])
+          navigate('/userDashboard');
+        }
+      },[userInfo,navigate])
     
 
     return(
         <>
         <Container>
+
         <h1>Login</h1>
-        {/* <Form onSubmit={handleSubmit}> */}
-        <Form onSubmit={console.log("Hold until redux is ready")}>
+        <Form onSubmit={handleSubmit}>
+        {/* <Form onSubmit={console.log("Hold until redux is ready")}> */}
             <Form.Group className="py-2" controlId="email">
                 <Form.Label>Email Address</Form.Label>
                 <Form.Control

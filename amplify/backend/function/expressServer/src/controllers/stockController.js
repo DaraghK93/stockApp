ba7cdffc4519b7f1d925cf4d4d9b4626363e753f
Caolán Power <caolanpower@ucdconnect.ie>
2022-10-04 15:30:25 +0100

    Added routes for creating and retrieving stocks.

--- amplify/backend/function/expressServer/src/controllers/stockController.js
+++ amplify/backend/function/expressServer/src/controllers/stockController.js
@@ -1,14 +1,89 @@
+const { compileString } = require('sass');
+const Stock = require('../models/stock.model');
 
 
 // @desc get individual stock price
 // @route GET /api/stock/price/:name
 // @access Public
 
-const getStockPrice = (req,res) => {
-    res.json({ message: 'The stock price is 200' })
+const getStockPrice = async (req,res) => {
+    try {
+        console.log(req.params.name);
+        
+        const stock = await Stock.find({name: req.params.name});
+        
+        if (!stock.length){
+            return res.status(404).json({msg: 'Stock not found'});
+        }
+
+        res.json(stock[0].price);
+    }catch (err){
+        console.error(err.message);
+        res.status(500).send('Server error');
+    }
+    
+}
+
+// @route   GET api/stocks
+// @desc    Get all stocks
+// @access  Private - add auth middleware to make it private
+const getAllStocks = async (req,res) =>{
+    try {
+        const stocks = await Stock.find();
+        res.json(stocks);
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send('Server Error');
+    }
+}
+
+
+// @desc get individual stock price
+// @route POST /api/stock/addStock/
+// @access Public
+
+const addStock = async (req,res) => {
+    try {
+        console.log(req.params.name);
+        
+        const newStock = new Stock({
+            name: req.body.name,
+            price: req.body.price
+        });
+
+        const stock = await newStock.save();
+
+        res.json({ stock }) 
+    }catch (err){
+        console.error(err.message);
+        res.status(500).send('Server error');
+    }
+    
+}
+
+
+
+// @desc get individual stock price
+// @route PUT /api/stock/updatestock/:name
+// @access Public
+
+const updateStock = async (req,res) => {
+    try{
+        console.log(req.params.name)
+
+        const stock = Stock.find({symbol: req.params.name})
+
+        const newStock = req.params.name
+
+
+        res.json(req.params.name);
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send('Server error');
+    }
 }
 
 
 module.exports = {
-    getStockPrice
+    getStockPrice, updateStock, addStock, getAllStocks
 } 
--- amplify/backend/function/expressServer/src/models/stock.model.js
+++ amplify/backend/function/expressServer/src/models/stock.model.js
@@ -1,25 +1,26 @@
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
 
-const stockSchema = new mongoose.schema(
+const stockSchema = new mongoose.Schema(
   {
-    exchange: {type: String, trim: true, required: true},
-    symbol: {type: String, trim: true,required: true},
-    shortname: {type: String, trim: true,required: true},
-    longname: {type: String, trim: true,required: true},
-    sector: {type: String, trim: true,required: true},
-    industry: {type: String, trim: true,required: true},
-    currentprice: {type: String, trim: true,required: true},
-    marketcap: {type: String, trim: true,required: true},
-    ebitda: {type: String, trim: true,required: true},
-    revenuegrowth: {type: String, trim: true,required: true},
-    city: {type: String, trim: true,required: true},
-    state: {type: String, trim: true,required: true},
-    country: {type: String, trim: true,required: true},
-    fulltimeemployees: {type: String, trim: true,required: true},
-    businessummary: {type: String, trim: true,required: true},
+    // exchange: {type: String, trim: true, required: true},
+    name: {type: String,required: true},
+    // shortname: {type: String, trim: true,required: true},
+    // longname: {type: String, trim: true,required: true},
+    // sector: {type: String, trim: true,required: true},
+    // industry: {type: String, trim: true,required: true},
+    price: {type: String,required: true}
+    // marketcap: {type: String, trim: true,required: true},
+    // ebitda: {type: String, trim: true,required: true},
+    // revenuegrowth: {type: String, trim: true,required: true},
+    // city: {type: String, trim: true,required: true},
+    // state: {type: String, trim: true,required: true},
+    // country: {type: String, trim: true,required: true},
+    // fulltimeemployees: {type: String, trim: true,required: true},
+    // businessummary: {type: String, trim: true,required: true},
   }
-)
+);
 
 const model = mongoose.model('StockData', stockSchema)
 
-module.exports = model
+
+module.exports = Stock = mongoose.model('stock', stockSchema);

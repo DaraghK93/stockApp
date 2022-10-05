74328774b0c167327b8276772bf738ef827628e4
Caolán Power <caolanpower@ucdconnect.ie>
2022-10-05 16:42:47 +0100

    Added error middleware and changed controller errors to match convention

--- amplify/backend/function/expressServer/src/app.js
+++ amplify/backend/function/expressServer/src/app.js
@@ -1,4 +1,3 @@
-
 /*
 Copyright 2017 - 2017 Amazon.com, Inc. or its affiliates. All Rights Reserved.
 Licensed under the Apache License, Version 2.0 (the "License"). You may not use this file except in compliance with the License. A copy of the License is located at
@@ -6,38 +5,44 @@ Licensed under the Apache License, Version 2.0 (the "License"). You may not use 
 or in the "license" file accompanying this file. This file is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and limitations under the License.
 */
-const port = 3000
-const express = require('express')
-const bodyParser = require('body-parser')
-const awsServerlessExpressMiddleware = require('aws-serverless-express/middleware')
-const connectDB = require('./config/db')
+const port = 3000;
+const express = require('express');
+const bodyParser = require('body-parser');
+const awsServerlessExpressMiddleware = require('aws-serverless-express/middleware');
+const connectDB = require('./config/db');
 // declare a new express app
-const app = express()
+const app = express();
 const cors = require('cors');
+const { errorHandler } = require('./middleware/errorMiddleware');
 
 connectDB();
 
-app.use(bodyParser.json())
-app.use(awsServerlessExpressMiddleware.eventContext())
+app.use(bodyParser.json());
+app.use(awsServerlessExpressMiddleware.eventContext());
+app.use(express.json());
 
 // Enable CORS for all methods
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*")
-  res.header("Access-Control-Allow-Headers", "*")
-  next()
+app.use(function (req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', '*');
+  next();
 });
 
-app.use(cors())
-app.use('/api/user', require("./routes/userRoutes"))
-app.use('/api/stock', require("./routes/stockRoutes"))
 
 
-app.listen(port, function() {
-  console.log(`App started on port ${port}`)
+app.use(cors());
+app.use('/api/user', require('./routes/userRoutes'));
+app.use('/api/stock', require('./routes/stockRoutes'));
+
+
+// Custom Middleware here
+app.use(errorHandler);
+
+app.listen(port, function () {
+  console.log(`App started on port ${port}`);
 });
 
 // Export the app object. When executing the application local this does nothing. However,
 // to port it to AWS Lambda we will create a wrapper around that will load the app from
 // this file
-module.exports = app
-
+module.exports = app;
--- amplify/backend/function/expressServer/src/controllers/userController.js
+++ amplify/backend/function/expressServer/src/controllers/userController.js
@@ -8,142 +8,151 @@ const config = require('config');
 // @route POST /api/users
 // @access Public
 
-const registerUser = async (req,res) => {
+const registerUser = async (req, res, next) => {
+  // may need to put some validation here, although it's already on the frontend and in the models
+  const { name, email, username, password } = req.body;
+  // console.log(name)
+  try {
+    let user = await User.findOne({ email });
 
-   // may need to put some validation here, although it's already on the frontend and in the models
-        const {name,email,username,password} = req.body;
-        // console.log(name)
-        try {
-            let user = await User.findOne({ email });
-    
-            if(user) {
-                return res.status(400).json({ error: 'User already exists'});
-            }
-    
-            user = new User({
-                name,
-                username,
-                email,
-                password
-            });
-            let salt = await bcrypt.genSalt(10);
-            user.password = await bcrypt.hash(password, salt);
-    
-            await user.save();
-            console.log(user)
-            const payload = {
-                user: {
-                    user: {
-                        id: user.id
-                    }
-                }
-            }
-    
-            jwt.sign(payload,
-             config.get('jwtSecret'), // need to set config in .env this is throwing an error for now
-            { expiresIn: 360000},
-            (err, token) => {
-                if(err) throw err;
-                res.json({token});
-            });
-    
-        } catch (err) {
-            if(User) {
-                console.error(err.message);
-                return res.status(500).send('Server error');
-    
-            }
-        }
+    if (user) {
+      // User already exists
+      res.status(400);
+      res.errormessage = 'User already exists';
+      return next(new Error('User already exists'));
+    }
+
+    user = new User({
+      name,
+      username,
+      email,
+      password,
+    });
+    let salt = await bcrypt.genSalt(10);
+    user.password = await bcrypt.hash(password, salt);
+
+    await user.save();
+    console.log(user);
+    const payload = {
+      user: {
+        user: {
+          id: user.id,
+        },
+      },
     };
 
+    jwt.sign(
+      payload,
+      config.get('jwtSecret'), // need to set config in .env this is throwing an error for now
+      { expiresIn: 360000 },
+      (err, token) => {
+        if (err) throw err;
+        res.json({ token });
+      }
+    );
+  } catch (err) {
+    if (User) {
+      console.error(err.message);
+      res.errormessage = 'Server error';
+      return next(err);
+    }
+  }
+};
 
 //@desc Authenticate USer
 // route POST /api/users/login
 // @access Public
 
-const loginUser = async (req,res) => {
-    const {email, password} = req.body
-    try{
-        let user = await User.findOne({ email });
+const loginUser = async (req, res) => {
+  const { email, password } = req.body;
+  try {
+    let user = await User.findOne({ email });
 
-        if(!user) {
-        return res
-            .status(400)
-            .json({ errors: [{ msg: 'Invalid credentials' }]  });
-        }
-
-        const isMatch = await bcrypt.compare(password, user.password);
-
-        if (!isMatch) {
-        return res
-            .status(400)
-            .json({ errors: [{ msg: 'Invalid credentials' }]  });
-        }
-
-        const payload = {
-            user: {
-                id: user.id
-            }
-        };
-
-        jwt.sign(payload,
-            config.get('jwtSecret'),
-            { expiresIn: 360000 },
-            (err, token) => {
-            if(err) throw err;
-            res.json({ token });
-        });
-    } catch(err) {
-        console.error(err.message);
-        res.status(500).send('Server error');
+    if (!user) {
+      res.status(400);
+      res.errormessage = 'Invalid credentials';
+      return next(new Error('Invalid credentials'));
     }
 
-}
+    const isMatch = await bcrypt.compare(password, user.password);
+
+    if (!isMatch) {
+      res.status(400);
+      res.errormessage = 'Invalid credentials';
+      return next(new Error('Invalid credentials'));
+    }
+
+    const payload = {
+      user: {
+        id: user.id,
+      },
+    };
+
+    jwt.sign(
+      payload,
+      config.get('jwtSecret'),
+      { expiresIn: 360000 },
+      (err, token) => {
+        if (err) throw err;
+        res.json({ token });
+      }
+    );
+  } catch (err) {
+    console.error(err.message);
+    res.errormessage = 'Server error';
+    return next(err);
+  }
+};
 
 // @desc get user data
 // route get /api/users/info
 // @access private
 
-const getUserInfo = async (req,res) => {
-
-    try {
+const getUserInfo = async (req, res) => {
+  try {
     // get all their details except their password
-      const userDetails = await User.findById(req.user.id).select('-password');
-  
-      if (!userDetails) {
-        return res.status(400).json({ msg: 'There is no profile for this user' });
-      }
-  
-      res.json(userDetails);
-    } catch (err) {
-      console.error(err.message);
-      res.status(500).send('Server Error');
-    }
-  };
+    const userDetails = await User.findById(req.user.id).select('-password');
 
+    if (!userDetails) {
+      res.status(400);
+      res.errormessage = 'There is no profile for this user';
+      return next(new Error('No profile for this user'));
+    }
+
+    res.json(userDetails);
+  } catch (err) {
+    console.error(err.message);
+    res.errormessage = 'Server error';
+    return next(err);
+  }
+};
 
 // @route    DELETE api/users/delete
 // @desc     Delete current user
 // @access   Private
-const deleteUser =  async (req, res) => {
-    try {
-      const user = await User.findByIdAndDelete(req.user.id).select('-password');
-      console.log(req.user.id);
-      console.log('user = ' +user);
-  
-      if (!user) {
-        return res.status(400).json({ msg: 'Can\'t find user' });
-      }
-      
-      res.json(user.name);
-    } catch (err) {
-      console.error(err.message);
-      res.status(500).send('Server Error');
-    }
-  }
+const deleteUser = async (req, res) => {
+  try {
+    const user = await User.findByIdAndDelete(req.user.id).select('-password');
+    console.log(req.user.id);
+    console.log('user = ' + user);
 
+    if (!user) {
+      res.status(404);
+      res.errormessage = 'User not found';
+      return next(new Error('User not found'));
+    }
+
+    res.json(user.name);
+  } catch (err) {
+    console.error(err.message);
+    res.errormessage = 'Server error';
+    return next(err);
+  }
+};
 
 module.exports = {
-    registerUser,loginUser, getUserInfo, deleteUser
-} 
-
+  registerUser,
+  loginUser,
+  getUserInfo,
+  deleteUser,
+};

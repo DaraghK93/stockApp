e90ddbd7a9a3aeae50594eaebcb5fe540d4f1e4f
Warren Kavanagh <user.name>
2022-10-06 19:17:56 +0100

    Changed the styling of header

--- src/components/layout/Header/header.js
+++ src/components/layout/Header/header.js
@@ -36,29 +36,27 @@ function Header() {
   }
 
 
-
-
   return (
-    <Navbar bg="light" expand="lg">
+    <Navbar expand="lg" style={{backgroundColor: 'dodgerblue', color:'white'}}>
       <Container>
-        <Navbar.Brand as={Link} to="/">FIN<span className="navbar-brand mb-0 h1">OPTIMIZE</span></Navbar.Brand>
+        <Navbar.Brand style={{color:'white'}} as={Link} to="/">FIN<span className="navbar-brand mb-0 h1">OPTIMIZE</span></Navbar.Brand>
         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
         <Navbar.Collapse id="responsive-navbar-nav">
-          <Nav className="me-auto">
-            <Nav.Link as={Link} to="/">Home</Nav.Link>
-            <Nav.Link as={Link} to="/stockdiscovery">Stocks</Nav.Link>
+          <Nav className="me-auto" >
+            <Nav.Link as={Link} style={{color:'white'}} to="/">Home</Nav.Link>
+            <Nav.Link as={Link} style={{color:'white'}} to="/stockdiscovery">Stocks</Nav.Link>
           </Nav>
           <Nav>
              {userInfo ?
-              (<NavDropdown align="end" title={userInfo.firstname}>
-                <NavDropdown.Item as={Link} to="/profile">My Profile</NavDropdown.Item>
-                <NavDropdown.Item as={Link} to="/portfolio">My Portfolio</NavDropdown.Item>
-                <NavDropdown.Item onClick={logoutHandler}>Logout</NavDropdown.Item>
+              (<NavDropdown  align="end" title={userInfo.firstname}>
+                <NavDropdown.Item as={Link}  to="/profile">My Profile</NavDropdown.Item>
+                <NavDropdown.Item as={Link}  to="/portfolio">My Portfolio</NavDropdown.Item>
+                <NavDropdown.Item onClick={logoutHandler} >Logout</NavDropdown.Item>
               </NavDropdown>)
               :
               <>
-              <Nav.Link as={Link} to="/register">Register</Nav.Link>
-              <Nav.Link as={Link} to="/login">Login</Nav.Link> 
+              <Nav.Link as={Link} to="/register" style={{color:'white'}}>Register</Nav.Link>
+              <Nav.Link as={Link} to="/login"style={{color:'white'}}>Login</Nav.Link> 
               </>
              }
           </Nav>

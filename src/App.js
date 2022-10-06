7230a51680f651b4ef4e648540d15b19f44c6b07
Warren Kavanagh <user.name>
2022-10-06 18:58:01 +0100

    Created a new component called FormContainer which can be used to hold forms in the website. Gives them consistent breakpoints

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import './App.css';
-import {BrowserRouter as Router,Routes,Route,Navigate } from 'react-router-dom'
+import {BrowserRouter as Router,Routes,Route} from 'react-router-dom'
 import Header from './components/layout/Header/header'
-import Footer from './components/layout/Footer/footer'
 import RegistrationPage from './screens/registration/registrationScreen'
 import LoginPage from './screens/login/loginscreen'
 
@@ -9,7 +8,6 @@ import LoginPage from './screens/login/loginscreen'
 
 function App() {
   return (
-    
     <Router> 
       <Header />
       <Routes>

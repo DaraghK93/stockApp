7e23c0325710b156b0a85a49651221dca5f42a77
Warren Kavanagh <user.name>
2022-10-08 20:39:30 +0100

    Added the feedparser as a dependency. There is now a function to get the feeds data, still needs to be parsed. This may require some work as all the feeds do not seem to have entirely common structure. Some do also not have images so a default thumbnail will need to be obtained for these

--- amplify/backend/function/newsArticleScrapingAndSentimentAnalysis/src/index.py
+++ amplify/backend/function/newsArticleScrapingAndSentimentAnalysis/src/index.py
@@ -7,6 +7,8 @@
 ### Imports ###
 import json
 import os 
+import feedparser
+from pprint import pprint
 
 
 
@@ -21,6 +23,8 @@ dir_path = os.path.dirname(os.path.realpath(__file__))
 #   This function reads in a supplied JSON file of RSS sources and returns a dictionary
 ## Inputs:
 #   file - Path to JSON file 
+## Return
+#   contnent - An array of dictionaries containing RSS feeds info 
 def readSources(file):
     try:
         f = open(file)
@@ -30,15 +34,42 @@ def readSources(file):
     except Exception as e:
         print(f'Error occured in trying to read file supplied "{file}"\nException message {e}')
 
+### getRSSFeed ###
+## Description:
+#   Calls a get request to the 
+def getRSSFeed(url):
+    try:
+        data = feedparser.parse(url)
+        return data
+    except Exception as e:
+        print(f'Cannot get RSS feed from the URL:{url}.\nException Details:\n{e}')
+    #print(f'Title:\t{data.entries[0].title}')
+    #print(f'Link:\t{data.entries[0].link}')
+    #print(f'Description:\t{data.entries[0].description}')
+    #print(f'Date:\t{data.entries[0].published}')
+    #print(f'Date Parsed:\t{data.entries[0].published_parsed}')
+    #print(f'GUID:\t{data.entries[0].id}')
+
+
+#def parseRSSFeed()
+
+
 
 
 ### Handler ###
 def handler(event, context):
     ## Step One ###
-    #   Parse the RSS feeds 
+    #   Get the feeds source, topic and URL 
     rssSources = readSources(f'{dir_path}\lib\RSSSources.json')
-    print(rssSources)
-    
+    #   For each spurce URL make a get request to get the feeds data  
+    for source in rssSources:
+        print("*************************")
+        print(source["Source"])
+        feedData = getRSSFeed(source["URL"])
+        pprint(feedData.entries[0])
+    #getRSSFeed(rssSources[0]["URL"])
+
+
     ## Step Two ##
     #   Get the features required for machine learning classifier 
 

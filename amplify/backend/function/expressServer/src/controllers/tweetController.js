0c28d959c475a71915daa48b2d453ca9e1f12124
Caolán Power <d21125982@mytudublin.ie>
2022-10-10 09:32:44 +0100

    Clean up tweet controller

--- amplify/backend/function/expressServer/src/controllers/tweetController.js
+++ amplify/backend/function/expressServer/src/controllers/tweetController.js
@@ -32,7 +32,6 @@ const addTweet = async (req, res, next) => {
 const getTweets = async (req, res, next) => {
   try {
     const tweet = await Tweet.find({ stock: req.params.stock });
-    console.log(req.params.stock);
     if (!tweet.length) {
       // No stock found
       res.status(404);

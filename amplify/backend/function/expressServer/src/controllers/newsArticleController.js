f5b3048e800bd83dbb74c2eaadadc18b016376bd
Warren Kavanagh <user.name>
2022-10-12 18:19:54 +0100

    Need to clean up the searching. Problme with Inc. being in all of the company names. Has to be removed and regex cleaned up

--- amplify/backend/function/expressServer/src/controllers/newsArticleController.js
+++ amplify/backend/function/expressServer/src/controllers/newsArticleController.js
@@ -1,10 +1,32 @@
 /// Description ///
 //  This file holds all the controllers for the route /api/newsarticles
 
+/// Imports ///
+// 
+const Article = require('../models/article.model');
+
 
 const getNewsArticles = async (req, res, next) => { 
     try{
-        res.json("Test")
+        const symbol    = req.query.symbol
+        const shortname = req.query.shortname
+        const longname  = req.query.longname
+        /// if these querys are undefined just return all articles
+        if (typeof symbol === 'undefined' && typeof shortname === 'undefined' && typeof longname === 'undefined'){
+            const articles = await Article.find({});
+            res.json(articles)
+        }else{
+            /// For the logname and shortname if it contains "Inc" remove it 
+            const shortnameNoInc = shortname.replace(/Inc\./,'')
+            const longnameNoInc = longname.replace(/Inc\./,'')
+            console.log(shortnameNoInc)
+            const articles = await Article.find(
+                {"headline":{$regex:shortnameNoInc,$options: 'i'}}
+            )
+            res.json(articles)
+        }
+       
+        
     }catch (err) {
         console.error(err.message);
         res.errormessage = 'Server error';

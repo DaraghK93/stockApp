1845ca4759dfa21d7b444566283719b0a1c140ea
Bearach Byrne <C15379616@mytudublin.ie>
2022-10-17 15:41:06 +0100

    DOB field removed, tickbox added for age verif.

--- amplify/backend/function/expressServer/src/controllers/userController.js
+++ amplify/backend/function/expressServer/src/controllers/userController.js
@@ -1,205 +1,230 @@
-const User = require('../models/user.model');
-const jwt = require('jsonwebtoken');
-const bcrypt = require('bcryptjs');
-const config = require('config');
-
+const User = require('../models/user.model')
+const jwt = require('jsonwebtoken')
+const bcrypt = require('bcryptjs')
+const config = require('config')
 
 // @desc Register new user
 // @route POST /api/users
 // @access Public
 
 const registerUser = async (req, res, next) => {
- 
-  
   try {
-    // Parse the body 
+    // Parse the body
     const {
       firstname,
       lastname,
       email,
       username,
       password,
-      dob,
-      location,
-    } = req.body; 
-  
+      overeighteen,
+      // dob,
+      // location,
+    } = req.body
+
     // check for nulls. all fields must be filled
-    if(typeof firstname ==='undefined' || typeof lastname === 'undefined' || typeof email === 'undefined'
-     || typeof username ==='undefined' || typeof password ==='undefined' || typeof dob ==='undefined' 
-     || typeof location ==='undefined' )
-      
-     {
-      // data is missing bad request 
-      res.status(400);
-      res.errormessage ="All details are needed for a user to register";
-      return next(new Error("The client has not sent the required information to register the user"));
+    if (
+      typeof firstname === 'undefined' ||
+      typeof lastname === 'undefined' ||
+      typeof email === 'undefined' ||
+      typeof username === 'undefined' ||
+      typeof password === 'undefined'
+
+      // typeof dob === 'undefined'
+      // typeof location === 'undefined'
+    ) {
+      // data is missing bad request
+      res.status(400)
+      res.errormessage = 'All details are needed for a user to register'
+      return next(
+        new Error(
+          'The client has not sent the required information to register the user',
+        ),
+      )
+    } else if (typeof overeighteen == false) {
+      res.status(400)
+      res.errormessage = 'You must show that you are over 18'
     }
 
-    let user = await User.findOne({ email });
+    let user = await User.findOne({ email })
 
-    // Check for existing user 
+    // Check for existing user
     if (user) {
-      res.status(400);
-      res.errormessage = 'User already exists';
-      return next(new Error('User already exists'));
+      res.status(400)
+      res.errormessage = 'User already exists'
+      return next(new Error('User already exists'))
+    }
+    // Check if user ticked over eighteen checkbox
+    else if (overeighteen === 'false') {
+      res.status(400)
+      res.errormessage = 'User is not over 18'
+      return next(new Error('User is not over 18'))
     }
 
-    // Create a new user 
+    // Create a new user
     user = new User({
       firstname,
       lastname,
       username,
       email,
       password,
-      dob,
-      location
-    });
+      overeighteen,
+      // location,
+    })
 
     /// Hash the password ///
-    let salt      = await bcrypt.genSalt(10);
-    user.password = await bcrypt.hash(password, salt);
+    let salt = await bcrypt.genSalt(10)
+    user.password = await bcrypt.hash(password, salt)
 
-    // Save user in DB 
-    await user.save();
+    // Save user in DB
+    await user.save()
     const payload = {
       user: {
         user: {
           id: user.id,
         },
       },
-    };
+    }
 
-    jwt.sign( payload, config.get('jwtSecret'), {expiresIn:'360000'}, (err, token) => {
-      if (err) throw err;
-      res.json({
-        firstname,
-        lastname,
-        email,
-        username,
-        token,
-      })
-    })
-
+    jwt.sign(
+      payload,
+      config.get('jwtSecret'),
+      { expiresIn: '360000' },
+      (err, token) => {
+        if (err) throw err
+        res.json({
+          firstname,
+          lastname,
+          email,
+          username,
+          token,
+        })
+      },
+    )
   } catch (err) {
     if (User) {
-      console.error(err.message);
-      res.errormessage = 'Server error';
-      return next(err);
+      console.error(err.message)
+      res.errormessage = 'Server error'
+      return next(err)
     }
   }
-};
+}
 
 //@desc Authenticate User
 // route POST /api/users/login
 // @access Public
 
 const loginUser = async (req, res, next) => {
-  const { email, password } = req.body;
+  const { email, password } = req.body
 
   // ensure they added email and password
-  if(typeof email ==='undefined' || typeof password === 'undefined'){
-    // data is missing 
-    res.status(400);
-    res.errormessage ="An email and password is required to login";
-    return next(new Error("The client has not sent the required email and password information")); }
+  if (typeof email === 'undefined' || typeof password === 'undefined') {
+    // data is missing
+    res.status(400)
+    res.errormessage = 'An email and password is required to login'
+    return next(
+      new Error(
+        'The client has not sent the required email and password information',
+      ),
+    )
+  }
 
   try {
-    let user = await User.findOne({ email });
+    let user = await User.findOne({ email })
 
     if (!user) {
-      res.status(404);
-      res.errormessage = 'Invalid credentials';
-      return next(new Error('Invalid credentials'));
+      res.status(404)
+      res.errormessage = 'Invalid credentials'
+      return next(new Error('Invalid credentials'))
     }
 
-    const isMatch = await bcrypt.compare(password, user.password);
+    const isMatch = await bcrypt.compare(password, user.password)
 
     if (!isMatch) {
-      res.status(400);
-      res.errormessage = 'Invalid credentials';
-      return next(new Error('Invalid credentials'));
+      res.status(400)
+      res.errormessage = 'Invalid credentials'
+      return next(new Error('Invalid credentials'))
     }
     const payload = {
       user: {
         id: user.id,
       },
-    };
+    }
 
     jwt.sign(
       payload,
       config.get('jwtSecret'),
       { expiresIn: 360000 },
       (err, token) => {
-        if (err) throw err;
+        if (err) throw err
         res.json({
           firstname: user.firstname,
           lastname: user.lastname,
           email,
           username: user.username,
           dob: user.dob,
-          location: user.location,
+          // location: user.location,
           image: user.image,
           bio: user.bio,
           token,
-        });
-      }
-    );
+        })
+      },
+    )
   } catch (err) {
-    console.error(err.message);
-    res.errormessage = 'Server error';
-    return next(err);
+    console.error(err.message)
+    res.errormessage = 'Server error'
+    return next(err)
   }
-};
+}
 
 // @desc get user data
 // route get /api/users/info
 // @access private
 
-const getUserInfo = async (req, res,next) => {
+const getUserInfo = async (req, res, next) => {
   try {
     // get all their details except their password
-    const userDetails = await User.findById(req.user.id).select('-password');
+    const userDetails = await User.findById(req.user.id).select('-password')
 
     if (!userDetails) {
-      res.status(400);
-      res.errormessage = 'There is no profile for this user';
-      return next(new Error('No profile for this user'));
+      res.status(400)
+      res.errormessage = 'There is no profile for this user'
+      return next(new Error('No profile for this user'))
     }
 
-    res.json(userDetails);
+    res.json(userDetails)
   } catch (err) {
-    console.error(err.message);
-    res.errormessage = 'Server error';
-    return next(err);
+    console.error(err.message)
+    res.errormessage = 'Server error'
+    return next(err)
   }
-};
+}
 
 // @route    DELETE api/users/delete
 // @desc     Delete current user
 // @access   Private
-const deleteUser = async (req, res,next) => {
+const deleteUser = async (req, res, next) => {
   try {
-    const user = await User.findByIdAndDelete(req.user.id).select('-password');
-    console.log(req.user.id);
-    console.log('user = ' + user);
+    const user = await User.findByIdAndDelete(req.user.id).select('-password')
+    console.log(req.user.id)
+    console.log('user = ' + user)
 
     if (!user) {
-      res.status(404);
-      res.errormessage = 'User not found';
-      return next(new Error('User not found'));
+      res.status(404)
+      res.errormessage = 'User not found'
+      return next(new Error('User not found'))
     }
 
-    res.json(user.name);
+    res.json(user.name)
   } catch (err) {
-    console.error(err.message);
-    res.errormessage = 'Server error';
-    return next(err);
+    console.error(err.message)
+    res.errormessage = 'Server error'
+    return next(err)
   }
-};
+}
 
 module.exports = {
   registerUser,
   loginUser,
   getUserInfo,
   deleteUser,
-};
+}
--- src/actions/userActions.js
+++ src/actions/userActions.js
@@ -1,89 +1,99 @@
 // Description:
-//  This file holds all the redux actions associated with the user state 
+//  This file holds all the redux actions associated with the user state
 
 /// Imports ///
-// userActionConstatns - Contants for user actions 
-// APIName             - The name of the API 
-import {  
-    USER_LOGIN_REQUEST,USER_LOGIN_SUCCESS,USER_LOGIN_FAIL, USER_LOGOUT, // For userLoginLogoutReducer 
-    USER_REGISTER_REQUEST, USER_REGISTER_SUCCESS, USER_REGISTER_FAIL
-} from "../constants/userActionConstants";
-import {APIName} from '../constants/APIConstants';
-import { API } from "aws-amplify";
+// userActionConstatns - Contants for user actions
+// APIName             - The name of the API
+import {
+  USER_LOGIN_REQUEST,
+  USER_LOGIN_SUCCESS,
+  USER_LOGIN_FAIL,
+  USER_LOGOUT, // For userLoginLogoutReducer
+  USER_REGISTER_REQUEST,
+  USER_REGISTER_SUCCESS,
+  USER_REGISTER_FAIL,
+} from '../constants/userActionConstants'
+import { APIName } from '../constants/APIConstants'
+import { API } from 'aws-amplify'
 
-export function login (email,password) {
-    // Return an async function so that it can be awaited in component that calls it 
-    return async (dispatch) => {
-        try{
-            // Dispatch user login request whuch sets loading to true so that loading screen can be set 
-            dispatch({type:USER_LOGIN_REQUEST})
-            // Configure the HTTP request 
-            let path = '/api/user/login'
-            let requestConfig = {
-                body: {
-                    email: email,
-                    password: password
-                }
-            }
-            // Sent the request to backend 
-            const data = await API.post(APIName,path,requestConfig)
-            // Dispatch the user success action 
-            dispatch({type:USER_LOGIN_SUCCESS,payload:data})
-            // Set the users data to local storage also 
-            localStorage.setItem('userInfo',JSON.stringify(data))
-        }catch(error){
-            dispatch({
-                type:USER_LOGIN_FAIL,
-                payload:error.response.data.errormessage /// THIS NEEDS TO BE AGRRED UPON WITH BACKEND GUYS SHOULD BE NICE ERROR MESSAGE 
-            })
-        }
+export function login(email, password) {
+  // Return an async function so that it can be awaited in component that calls it
+  return async (dispatch) => {
+    try {
+      // Dispatch user login request whuch sets loading to true so that loading screen can be set
+      dispatch({ type: USER_LOGIN_REQUEST })
+      // Configure the HTTP request
+      let path = '/api/user/login'
+      let requestConfig = {
+        body: {
+          email: email,
+          password: password,
+        },
+      }
+      // Sent the request to backend
+      const data = await API.post(APIName, path, requestConfig)
+      // Dispatch the user success action
+      dispatch({ type: USER_LOGIN_SUCCESS, payload: data })
+      // Set the users data to local storage also
+      localStorage.setItem('userInfo', JSON.stringify(data))
+    } catch (error) {
+      dispatch({
+        type: USER_LOGIN_FAIL,
+        payload: error.response.data.errormessage, /// THIS NEEDS TO BE AGRRED UPON WITH BACKEND GUYS SHOULD BE NICE ERROR MESSAGE
+      })
     }
+  }
 }
-
 
 export function logout() {
-    return (dispatch) => {
-        // remvoe the users info from local storage 
-        localStorage.removeItem('userInfo')
-        // Dispatch the logout action 
-        dispatch({type:USER_LOGOUT})
-    }
-
+  return (dispatch) => {
+    // remvoe the users info from local storage
+    localStorage.removeItem('userInfo')
+    // Dispatch the logout action
+    dispatch({ type: USER_LOGOUT })
+  }
 }
 
-
-export function registerUser(firstName,lastName,email,username,password,dob,location){
-    return async (dispatch) => {
-        try{
-            // Initiate the request 
-            dispatch({type:USER_REGISTER_REQUEST})
-            // Configure the HTTP request
-            let path = '/api/user/'
-            let requestConfig = {
-                body: {
-                    firstname: firstName,
-                    lastname: lastName,
-                    email: email,
-                    username: username,
-                    password: password,
-                    dob: dob,
-                    location:location
-                }
-            }
-            //// Sent the request to backend 
-            const data = await API.post(APIName,path,requestConfig)
-            //// Dispatch the user register success, sets register loading false 
-            dispatch({type:USER_REGISTER_SUCCESS});
-            //// Dispatch login success as this will set user state in redux 
-            dispatch({type:USER_LOGIN_SUCCESS,payload:data})
-            //// Set the user in localstorage 
-            localStorage.setItem('userInfo',JSON.stringify(data))
-        }catch(error){
-            // Error in regsitraing user set the message 
-            dispatch({
-                type:USER_REGISTER_FAIL,
-                payload:error.response.data.errormessage
-            })
-        }
+export function registerUser(
+  firstName,
+  lastName,
+  email,
+  username,
+  password,
+  overEighteen,
+) {
+  return async (dispatch) => {
+    try {
+      // Initiate the request
+      dispatch({ type: USER_REGISTER_REQUEST })
+      // Configure the HTTP request
+      let path = '/api/user/'
+      let requestConfig = {
+        body: {
+          firstname: firstName,
+          lastname: lastName,
+          email: email,
+          username: username,
+          password: password,
+          overeighteen: overEighteen,
+          // dob: dob
+          // location:location
+        },
+      }
+      //// Sent the request to backend
+      const data = await API.post(APIName, path, requestConfig)
+      //// Dispatch the user register success, sets register loading false
+      dispatch({ type: USER_REGISTER_SUCCESS })
+      //// Dispatch login success as this will set user state in redux
+      dispatch({ type: USER_LOGIN_SUCCESS, payload: data })
+      //// Set the user in localstorage
+      localStorage.setItem('userInfo', JSON.stringify(data))
+    } catch (error) {
+      // Error in regsitraing user set the message
+      dispatch({
+        type: USER_REGISTER_FAIL,
+        payload: error.response.data.errormessage,
+      })
     }
+  }
 }

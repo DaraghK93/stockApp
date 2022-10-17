ff248ade45dc1001f513383cd32e98378ade651b
Joey Corcoran <81182125+joeycor92@users.noreply.github.com>
2022-10-17 17:28:05 +0100

    Lambda function working

--- amplify/backend/function/stockPriceFunction/src/index.py
+++ amplify/backend/function/stockPriceFunction/src/index.py
@@ -6,9 +6,6 @@ import datetime
 import os
 import boto3
 
-# set path variable for use 
-dir_path = os.path.dirname(os.path.realpath(__file__))
-
 def connectToDB(URI):
     # connect to the mongo instance
     # return error if connection fails
@@ -30,8 +27,7 @@ def getSecret(secretName,region="eu-north-1"):
         return response
     except Exception as e:
         print(f'ERROR:Could not get secret in getSecret function.\nException Details:\n\t{e}')
-# client = MongoClient("mongodb+srv://joeycor:X867a684!@cluster0.xzwrimn.mongodb.net/?retryWrites=true&w=majority")
-# database = client.test
+
 
 def get_current_price():
   scrapeURL = "https://www.slickcharts.com/sp500"
@@ -42,33 +38,38 @@ def get_current_price():
   # Method for scraping a table taken from:
   # stack overflow https://stackoverflow.com/questions/23377533/python-beautifulsoup-parsing-table
   data = []
+
+  #  finds a table tag with attribute named here
   table = soup.find('table', attrs={'class': 'table table-hover table-borderless table-sm'})
   table_body = table.find('tbody')
 
+  # loops through each row of the table and find all of the data and puts it in an array
   rows = table_body.find_all('tr')
   for row in rows:
       cols = row.find_all('td')
       cols = [ele.text.strip()for ele in cols]
       data.append(cols)
-      if len(data) > 500:
+      if len(data) > 495:
         break
 
   output = {}
   for i in range(0, len(data)):
-    output[data[i][2]] = data[i][4]
+    # add to dictionary in the form Symbol: Price
+    output[data[i][2]] = {"currentprice": data[i][4], "absoluteChange": data[i][5], "percentageChange": data[i][6].strip("()")}
   return output
 
 def create_data_request(data_curr_price, time_stamp):
-  # all_data = {}
   data_request = []
-  # name = "Prices." + time_stamp
+  name = "prices." + time_stamp
   for i in data_curr_price:
     try:
       if i == "BRK.B":
         i_new = "BRK-B"
-        data_request.append(UpdateOne({"Symbol": i_new}, {'$set': {"Prices.currentprice": {"time": time_stamp, "4. close": data_curr_price[i]}}}))
+        data_request.append(UpdateOne({"symbol": i_new}, {'$set': {"daily_change": data_curr_price[i_new]}}))
+        data_request.append(UpdateOne({"symbol": i_new}, {'$set': {name: {"time": time_stamp, "4. close": data_curr_price[i]["currentprice"]}}}))
       else:
-        data_request.append(UpdateOne({"Symbol": i}, {'$set': {"Prices.currentprice": {"time": time_stamp, "4. close": data_curr_price[i]}}}))
+        data_request.append(UpdateOne({"symbol": i}, {'$set': {"daily_change": data_curr_price[i]}}))
+        data_request.append(UpdateOne({"symbol": i}, {'$set': {name: {"time": time_stamp, "4. close": data_curr_price[i]["currentprice"]}}}))
     except KeyError:
       continue
   return data_request
@@ -76,13 +77,22 @@ def create_data_request(data_curr_price, time_stamp):
 
 def handler(event,context):
   try:
-    client = MongoClient("mongodb+srv://joeycor:X867a684!@cluster0.xzwrimn.mongodb.net/?retryWrites=true&w=majority")
-    db = client.test
+    # get the URI from secret
+    mongoURI = getSecret('MONGO_URI').get('Parameter').get('Value')
+
+    # connect to DB
+    client = connectToDB(mongoURI)
+    
+
+    # specify db and collection
+    db = client[os.environ["DATABASE"]]
+    collection = db[os.environ["COLLECTION"]]
   except Exception as e:
     print(f'ERROR:Error encountered in handler function.\nException Details:\n\t{e}')
     return {
         'Message': 'Error encountered, please view cloudwatch logs for detailied error messages',
     }   
+
 
   # get the current time and convert to the correct format YYYY-MM-DDTHH:MM:SS
   time_stamp = datetime.datetime.now()
@@ -91,43 +101,14 @@ def handler(event,context):
   # run the web scraping program to get the current prices
   data_current_price = get_current_price()
 
-  # get data from all stocks
-  all_stocks_data = db.sample_stock_data.find({})
-  data_request_current_price_to_timestamp = []
-
-  # loop through the data, going through each stock symbol
-  for i in all_stocks_data:
-    try:
-      # due to issues with BRK.B on slickcharts and BRK-B everywhere else this has to be done separately
-      if i["Symbol"] == "BRK.B":
-        i["Symbol"] = "BRK-B"
-      
-      # here we create our timestamp which will be used so that the format is correct
-      # correct format: Prices.time_stamp_key: previous current price
-      time_stamp_key = "Prices." + i["Prices"]["currentprice"]["time"]
-
-      # each of these is added to an array which will be utilised in the bulk write
-      data_request_current_price_to_timestamp.append(UpdateOne({"Symbol": i["Symbol"]}, {'$set': {time_stamp_key: {"4. close": i["Prices"]["currentprice"]["4. close"]}}}))
-    except KeyError:
-      continue
+  
   
   try:
 
-        # get the URI from secret
-        # mongoURI = getSecret('MONGO_URI').get('Parameter').get('Value')
-
-        # connect to DB
-        # client = connectToDB(mongoURI)
-        
-
-        # specify db and collection
-        # db = client[os.environ["DATABASE"]]
-        # collection = db[os.environ["COLLECTION"]]
-
         # update the DB with the data from API
-        db.sample_stock_data.bulk_write(data_request_current_price_to_timestamp)
+        # db.sample_stock_data.bulk_write(data_request_current_price_change)
         requests_database = create_data_request(data_current_price, time_stamp_str)
-        db.sample_stock_data.bulk_write(requests_database)
+        collection.bulk_write(requests_database)
 
         return {
             'statusCode': 200,

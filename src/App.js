e71b5e5839663ac7530af8ea20623da7c5fb2ea8
Caolán Power <d21125982@mytudublin.ie>
2022-10-18 16:37:04 +0100

    Cleanup App.js from old commit

--- src/App.js
+++ src/App.js
@@ -10,16 +10,6 @@ import TwitterFeed from './screens/TwitterFeed/TwitterFeed';
 
 function App() {
   return (
-    // <>
-    //   <div>
-    //     <TwitterTweetEmbed tweetId={'1580112905705787392'} />
-    //     <TwitterTweetEmbed tweetId={'1580137809544769539'} />
-    //     <TwitterTweetEmbed tweetId={'1580136679377338370'} />
-    //     <TwitterTweetEmbed tweetId={'1580121369932488705'} />
-    //     <TwitterTweetEmbed tweetId={'1580056072764608513'} />
-    //     <TwitterTweetEmbed tweetId={'1580027349101223936'} />
-    //   </div>
-    // </>
     <Router>
       <Header />
       <Routes>
@@ -28,7 +18,6 @@ function App() {
         <Route path='/login' element={<LoginPage />} />
         <Route path='/stockdiscovery' element={<StockDiscoveryPage />} />
         <Route path='/stock/:symbol' element={<StockPage />} />
-        <Route path='/twitterfeed' element={<TwitterFeed />} />
       </Routes>
     </Router>
   );

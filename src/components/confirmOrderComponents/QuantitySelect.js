a1c82eb5bd54e53322203ee1551ad8156274a9b2
Rebecca Kelly <bex.r.kelly@gmail.com>
2022-10-25 12:39:17 +0100

    added new param display balance and actual balance

--- src/components/confirmOrderComponents/QuantitySelect.js
+++ src/components/confirmOrderComponents/QuantitySelect.js
@@ -4,13 +4,15 @@ import { useState, useEffect } from 'react';
 function QuantitySelect({ portfolioBalance, stockPrice }) {
     const [max, setMax] = useState();
     const [value, setValue] = useState(0);
+    const [displayBalance, setDisplaceBalance] = useState(portfolioBalance);
     const [balance, setBalance] = useState(portfolioBalance);
 
     useEffect(() => {
         if (portfolioBalance - (stockPrice * value) < 0) {
             setMax(value - 1)
         }
-        setBalance(portfolioBalance - (stockPrice * value))
+        setBalance((portfolioBalance - (stockPrice * value)).toFixed(3))
+        setDisplaceBalance((portfolioBalance - (stockPrice * value)).toFixed(2))
     }, [portfolioBalance, value])
 
     return (
@@ -20,7 +22,8 @@ function QuantitySelect({ portfolioBalance, stockPrice }) {
                     <h5>Quantity</h5>
                     <Row style={{ display: "flex", alignItems: "center", justifyContent: "center", marginBottom: "10px" }}>
                         <h2>Quantity: {value}</h2>
-                        <p>Your Balance: {balance}</p>
+                        <p><strong>Your Balance: </strong>{displayBalance}</p>
+                        <p>Balance sent to backend: {balance}</p>
                     </Row>
                     <input type="range" className="form-range" min={0} max={max} value={value}
                         onChange={e => setValue(e.target.value)}

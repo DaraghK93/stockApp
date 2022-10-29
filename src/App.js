ee53339d0e0fa68ce9ff790f3ff6cd85c2e52a95
Bearach Byrne <C15379616@mytudublin.ie>
2022-10-29 15:27:50 +0100

    Added notFound component as a 404 page

--- src/App.js
+++ src/App.js
@@ -6,12 +6,14 @@ import LoginPage from './screens/login/loginscreen';
 import StockDiscoveryPage from './screens/stockDiscovery/stockDiscoveryScreen';
 import StockPage from './screens/stockpage/StockPage';
 import OrderConfirmationPage from './screens/orderConfirmation/orderConfirmation';
+import NotFound from './screens/errorScreens/notFound';
 
 function App() {
   return (
     <Router>
       <Header />
       <Routes>
+        <Route path="*" element={<NotFound />} />
         <Route path='/' element={<RegistrationPage />} />
         <Route path='/register' element={<RegistrationPage />} />
         <Route path='/login' element={<LoginPage />} />
---
+++ src/screens/errorScreens/notFound.js
@@ -0,0 +1,25 @@
+import { useState, useEffect } from 'react'
+import { Form, Button, Row, Col } from 'react-bootstrap'
+import { useDispatch, useSelector } from 'react-redux'
+import { login } from '../../actions/userActions'
+import { useNavigate } from 'react-router-dom'
+import { Link } from 'react-router-dom'
+
+/// Widgets ///
+import MessageAlert from '../../components/widgets/MessageAlert/MessageAlert'
+import LoadingSpinner from '../../components/widgets/LoadingSpinner/LoadingSpinner'
+
+function NotFound() {
+  return (
+    <div>
+      <h1>Something went wrong!</h1>
+      <h2>We couldn't find this page.</h2>
+      <p>Here are some useful links:</p>
+      <Link to='/'>Home</Link>
+      <Link to='/stockdiscovery/'>Stock Discovery</Link>
+      <Link to='/register'>Registration</Link>
+    </div>
+  )
+
+}
+export default NotFound

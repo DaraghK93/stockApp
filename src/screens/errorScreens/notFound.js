f493d56fe715fa567e6e9f0c9d7eebd97472dc52
Bearach Byrne <C15379616@mytudublin.ie>
2022-10-30 15:10:19 +0000

    Fine tuning styling

--- src/screens/errorScreens/notFound.js
+++ src/screens/errorScreens/notFound.js
@@ -8,18 +8,12 @@ function NotFound() {
       <br></br>
       <h1>Something went wrong!</h1>
       <h2>We couldn't find this page.</h2>
-      <p>Here are some useful links:</p>
-      <ButtonGroup vertical>
+      <ButtonGroup vertical style={{margin:30}}>
         <Link to='/'><Button variant="secondary" className="notFoundButton btn btn-secondary btn-sm">Home</Button></Link>
         <Link to='/stockdiscovery/'><Button variant="secondary" className="notFoundButton btn btn-secondary btn-sm mr-1">Stock Discovery</Button></Link>
         <Link to='/register'><Button variant="secondary" className="notFoundButton btn btn-secondary btn-sm mr-1">Registration</Button></Link>
         <Link to='/login'><Button variant="secondary" className="notFoundButton btn btn-secondary btn-sm mr-1">Login</Button></Link>
       </ButtonGroup>
-
-      {/* <Link to='/'><Button variant="secondary" className="btn btn-secondary btn-sm">Home</Button></Link>
-      <Link to='/stockdiscovery/'><Button variant="secondary" className="notFoundButton btn btn-secondary btn-sm mr-1">Stock Discovery</Button></Link>
-      <Link to='/register'><Button variant="secondary" className="notFoundButton btn btn-secondary btn-sm mr-1">Registration</Button></Link>
-      <Link to='/login'><Button variant="secondary" className="notFoundButton btn btn-secondary btn-sm mr-1">Login</Button></Link> */}
     </div>
   )
 

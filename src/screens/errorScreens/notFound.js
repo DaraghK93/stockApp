7c5263235dceeb0856bef09c365bf0912d7acad7
Bearach Byrne <C15379616@mytudublin.ie>
2022-10-31 11:09:20 +0000

    Removed links, kept only home link

--- src/screens/errorScreens/notFound.js
+++ src/screens/errorScreens/notFound.js
@@ -10,9 +10,6 @@ function NotFound() {
       <h2>We couldn't find this page.</h2>
       <ButtonGroup vertical style={{margin:30}}>
         <Link to='/'><Button variant="secondary" className="notFoundButton btn btn-secondary btn-sm">Home</Button></Link>
-        <Link to='/stockdiscovery/'><Button variant="secondary" className="notFoundButton btn btn-secondary btn-sm mr-1">Stock Discovery</Button></Link>
-        <Link to='/register'><Button variant="secondary" className="notFoundButton btn btn-secondary btn-sm mr-1">Registration</Button></Link>
-        <Link to='/login'><Button variant="secondary" className="notFoundButton btn btn-secondary btn-sm mr-1">Login</Button></Link>
       </ButtonGroup>
     </div>
   )

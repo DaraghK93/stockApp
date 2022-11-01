07bf511a9f5c3505c5ff4bfda71f7208bb4234ce
Warren <warrenkavanagh1000@gmail.com>
2022-11-01 14:04:35 +0000

    Added the dependecies for the function to requirements.txt, sam build and local invoke still working

--- machineLearning/sentimentAnalysis/newsSentimentAnalysis/NewsSentimentLambdaFunction/newsSentiment/app.py
+++ machineLearning/sentimentAnalysis/newsSentimentAnalysis/NewsSentimentLambdaFunction/newsSentiment/app.py
@@ -1,16 +1,24 @@
+### Description ###
+#   This file is the main entry point to the lambda function 
+#   The function parses the RSS feeds for news articles 
+#   Classifies the sentiment of these articles as either positive, neutral or negative
+#   Then sends the data to the database 
+
+### Imports ###
 import json
-
-
-
-
-
-
-
-
-
-
-
-
+import os 
+import feedparser
+import boto3 
+import pymongo
+from pymongo.errors import DuplicateKeyError
+from datetime import datetime
+from datetime import timedelta
+from time import mktime
+from bs4 import  BeautifulSoup
+import pickle
+import csv
+from statistics import mean
+import nltk 
 
 
 #################################################################################################################

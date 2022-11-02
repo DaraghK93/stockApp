29aa2944a128d016dce6863bfd795629b8e70552
Warren <warrenkavanagh1000@gmail.com>
2022-11-02 12:18:46 +0000

    The model_1 for news sentiment now generating report for all metrics for the machine learning section

--- machineLearning/sentimentAnalysis/newsSentimentAnalysis/modelGeneration/model_1/main.py
+++ machineLearning/sentimentAnalysis/newsSentimentAnalysis/modelGeneration/model_1/main.py
@@ -86,17 +86,14 @@ if __name__ == "__main__":
     ### Step 6 - Evaluate the models and save them ###
     evaluations = [] 
     for name,classifier in classifiers.items():
-       
-        evaluation = modelFunctions.evaluateModel(classifier,test)
+        # Evaluate the model and add it to the evulations list 
+        evaluation = {}
         evaluation["model"] = name
-        print(f"For {name}, The results are:\n {evaluation}\n")
-        # Evaluation 
-        #accuracy= modelFunctions.getAccuracyofClassifier(classifier,test)
-        #evaluations.append({'classifier':name,'accuracy':f'{accuracy:.2%}'})
-        #print(F"{accuracy:.2%} - {name}")
+        evaluation.update(modelFunctions.evaluateModel(classifier,test))
+        evaluations.append(evaluation)
         # Save to pickle file
-        #modelFile = f'./models/{name}.pickle'
-        #modelFunctions.saveClassifier(classifier,modelFile)
+        modelFile = f'./models/{name}.pickle'
+        modelFunctions.saveClassifier(classifier,modelFile)
     # Write the results to a csv 
-    #evalFile = "evaluationResults.csv"
-    #modelFunctions.generateEvaluationReport(evaluations,evalFile)   
+    evalFile = "evaluationResults.csv"
+    modelFunctions.generateEvaluationReport(evaluations,evalFile)   

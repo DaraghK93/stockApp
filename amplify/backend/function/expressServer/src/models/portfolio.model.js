4b6472de99e63a7c53ee5fa6b1e85c474130e23e
Joey Corcoran <81182125+joeycor92@users.noreply.github.com>
2022-11-03 12:45:27 +0000

    Update portfolio schema to be separate to users

--- amplify/backend/function/expressServer/src/models/portfolio.model.js
+++ amplify/backend/function/expressServer/src/models/portfolio.model.js
@@ -9,7 +9,8 @@ const PortfolioSchema = new mongoose.Schema(
     dateCreated: {type: String, trim: true},
     totalValue: {type: String, trim: true, required: true},
     transactions: [{stock: {type: Schema.Types.ObjectID, ref: 'stock'}, units: {type:Number}, transactionType: {type: String}, date: {type: Date}}],
-    valueHistory: [{stock: {type: Schema.Types.ObjectID, ref: 'stock'}, units: {type:Number}, date: {type: Date}}]
+    valueHistory: [{stock: {type: Schema.Types.ObjectID, ref: 'stock'}, units: {type:Number}, date: {type: Date}}],
+    leagues: [{type: Schema.Types.ObjectID, ref: 'leagues'}]
   },
   { collection: 'portfolio-data' },
   // Timestamps used to create createdAt and updatedAt fields in the model that allows us to track when the entity was created/updated

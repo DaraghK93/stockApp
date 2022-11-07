649deebcae52f78084a36516dd74e483ba3d9215
Warren <warrenkavanagh1000@gmail.com>
2022-11-07 19:14:53 +0000

    Created component for the buttons for the selection on My Games and Join a Game

--- src/screens/gameScreen/gameScreen.js
+++ src/screens/gameScreen/gameScreen.js
@@ -1,18 +1,32 @@
 /// Description:
 //      This is the screen which is displayed when the user clicks into "game" on the nav bar 
+import { useState } from 'react'
+import { Container, Row, Col} from "react-bootstrap"
 
-import { Container, Button, Row, Col} from "react-bootstrap"
+//// Components ///
+import ScreenSelectionRadioButton from "../../components/gameComponents/gameScreenComponents/screenSelectionRadioButton/screenSelectionRadioButton";
+
+
 
 
 function GameScreen(){
+    // constant email holds the value of the input email address
+    const [screen, setScreen] = useState('1')
+
+
+    /// The choices for the screens, used for buttons at top o screen 
+    var screenChocies = [
+        {name: 'My Games', value:'1'},
+        {name: 'Join a Game', value: '2'}
+    ]
+
     return(
         <Container>
             <Row>
                 <Col><h1>GameScreen</h1></Col>
             </Row>
-            <Row>
-                <Col>My Games</Col>
-                <Col>Join a Game</Col>
+            <Row lg={1} md={1} xs={1}>
+                <ScreenSelectionRadioButton choices={screenChocies} state={screen} setter={setScreen}/>
             </Row>
             <Row>
                 <Col>Create a Game Component</Col>

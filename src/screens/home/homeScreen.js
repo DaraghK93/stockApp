951e71bac8a6a5186df9b24b864af21ac24b2cb9
Warren <warrenkavanagh1000@gmail.com>
2022-11-07 15:27:26 +0000

    The basic home page now has a button play now which will redirect the user to the register page when they click it

--- src/screens/home/homeScreen.js
+++ src/screens/home/homeScreen.js
@@ -5,13 +5,53 @@
 //  This screen contains the components redenred to the user when they first visit the page
 
 /// Imports ///
-import { Container, Row, Col } from 'react-bootstrap'
+import { Container, Row, Col, Button } from 'react-bootstrap'
+import {useNavigate} from 'react-router-dom';
 
 function HomeScreen (){
 
+    const navigate = useNavigate(); 
+
+
+    const navigateRegister = (e) => {
+        e.preventDefault()
+        navigate(`/register`)
+  }
+
+
+
     return(
         <Container>
-            <h1>Hello</h1>
+            <Col>
+                <Row>
+                    <h1>FinOptimse - Fantasy Stock Trading Game</h1>
+                </Row>
+                <Row>
+                    <Col>
+                        <Button onClick={navigateRegister}>Play Now</Button>
+                    </Col>
+                </Row>
+            </Col>
+            <Col>
+                <Row>
+                    <h2>About the Game</h2>
+                </Row>
+            </Col>
+            <Col>
+                <Row>
+                    <h2>Stock Leagues</h2>
+                </Row>
+            </Col>
+            <Col>
+                <Row>
+                    <h2>Intelligent Components</h2>
+                </Row>
+            </Col>
+            <Col>
+                <Row>
+                    <h2>Environmenatal Social and Governance (ESG) Investing</h2>
+                </Row>
+            </Col>
         </Container>
     )
 

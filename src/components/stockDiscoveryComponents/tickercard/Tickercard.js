bc1c6e57635f768c29438a493533bb8ce1959e0e
Rebecca Kelly <bex.r.kelly@gmail.com>
2022-11-07 17:14:30 +0000

    changed side scrollers, card styling, card holde3r

--- src/components/stockDiscoveryComponents/tickercard/Tickercard.js
+++ src/components/stockDiscoveryComponents/tickercard/Tickercard.js
@@ -2,6 +2,7 @@
 //  This component is used to display stocks to the user
 //  It is a card which shows an overview of the stock showing some key details 
 
+import { borderBottom } from '@mui/system';
 import { Card, ListGroupItem, ListGroup, Container } from 'react-bootstrap';
 
 import { Link } from "react-router-dom";
@@ -24,47 +25,62 @@ function TickerCard({ stock }) {
 
   return (
     <Link to={`/stock/${stock.symbol}`} style={{ textDecoration: 'none' }}>
-      <Card className="h-100 tickercardstyle" >
-        <ListGroup className="list-group-flush"
-          style={{
-            height: "5rem",
-            whiteSpace: "initial"
-          }}
-        >
-          <ListGroupItem >
-            <h5 style={{ float: "left" }}>{stock.longname}</h5>
-          </ListGroupItem>
-        </ListGroup>
-        <Container style={{ height: "11.25rem" }}>
+      <Card className="h-100 tickercardstyle" style={{
+        padding: "none"
+      }}>
+
+        <Container style={{ height: "100px", width: "150px" }}>
           <Card.Body style={{ textDecoration: 'none' }}>
             <Card.Img
               variant="top"
               src={stock.logo}
               style={{
                 display: "block",
-                width: "65%",
-                margin:"auto"
+                margin: "auto"
               }}
             />
           </Card.Body>
         </Container>
-        <ListGroup className="list-group-flush">
-          <ListGroupItem >
-
-            {stock.symbol} | {stock.exchange}
-
+        <ListGroup
+          className="list-group-flush"
+          style={{
+            borderTop: "none",
+            padding: "none",
+            borderBottom: "none",
+            border: "none"
+          }}>
+          <ListGroupItem
+            style={{
+              border: "none",
+              borderBottom: "none",
+              padding: "none",
+              margin: "none"
+            }}
+          ><h5>
+            <center>
+              {stock.symbol}</center></h5>
+              <span style={{ color: "grey", padding: "none", margin: "none", border: "none" }}><center>{stock.longname}</center></span>
           </ListGroupItem>
-          <ListGroupItem ><strong>Current price:</strong> ${stock.daily_change.currentprice}<br></br>
+
+          <ListGroupItem ><center><span style={{ color: redOrGreen(), border:"none" }}>{postiveSymbol}${stock.daily_change.absoluteChange} ({postiveSymbol}{stock.daily_change.percentageChange}%)</span>
+          </center>
+          </ListGroupItem>
+
+
+        </ListGroup>
+
+
+        {/* <ListGroupItem ><strong>Current price:</strong> ${stock.daily_change.currentprice}<br></br>
             <strong>Change (24h):</strong> <span style={{ color: redOrGreen() }}>${stock.daily_change.absoluteChange} ({postiveSymbol}{stock.daily_change.percentageChange}%)</span>
-          </ListGroupItem>
-          <ListGroupItem >
+          </ListGroupItem> */}
+        {/* <ListGroupItem >
             <img src="https://stockappnewslogobucket.s3.eu-west-1.amazonaws.com/twitter_logo_blue.png" alt="twitter symbol"
               style={{ width: "1.25rem" }} /> <strong>Twitter Sentiment: </strong><SentimentBadge sentiment='Positive' />
             <br></br>
             <img src="https://cdn-icons-png.flaticon.com/512/1042/1042782.png" alt="news symbol"
               style={{ width: "1.25rem" }} /> <strong>News Sentiment: </strong><SentimentBadge sentiment='Negative' />
-          </ListGroupItem>
-        </ListGroup>
+          </ListGroupItem> */}
+        {/* </ListGroup> */}
       </Card>
     </Link>
   );

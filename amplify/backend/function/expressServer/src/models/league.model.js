b064725372928651266107e7b20ec96e0ca184c9
Daragh Kneeshaw <daraghkneeshaw1@gmail.com>
2022-11-07 16:11:52 +0000

    formatting and sectors

--- amplify/backend/function/expressServer/src/models/league.model.js
+++ amplify/backend/function/expressServer/src/models/league.model.js
@@ -3,25 +3,93 @@ const {Schema} = mongoose
 
 const LeagueSchema = new mongoose.Schema(
     {
-      leagueName: { type: String, trim: true, required: true },
-      budget: { type: Number, trim: true, required: true},
-      leagueType: {type: String, enum: ["timeBased","valueBased"], required: true},
-      private: {type: Boolean, required: true},
-      active: {type: Boolean, required: true},
-      startDate: {type: Date, required: true },
-      endDate: {type: Date},
-      sectors: {type: [String]},
-      maxDailyTrades: {type:Number, required: true,  },
-      tradingFee: {type: Number, required: true},
-      minERating: {type: Number, default: 0},
-      minSRating: {type: Number, default: 0},
-      minGRating: {type: Number, default: 0},
-      leagueAdmin: {type: Schema.Types.ObjectId, ref: 'UserData'}, 
-      users: [{type: Schema.Types.ObjectId, ref: 'UserData'}], 
-      portfolios: [{type: Schema.Types.ObjectId, ref: 'PortfolioData'}]
+        leagueName: { 
+            type: String,
+            trim: true, 
+            required: true 
+        }, 
+        startingBalance: { 
+            type: Number, 
+            trim: true, 
+            required: true
+        }, 
+        leagueType: {
+            type: String,
+            enum: ["timeBased","valueBased"], 
+            required: true
+        },
+        private: {
+            type: Boolean, 
+            required: true
+        },
+        active: {
+            type: Boolean, 
+            required: true
+        }, // could we have a trigger that changes this depending on start/end?
+        startDate: {
+            type: Date, 
+            required: true 
+        }, 
+        endDate: {
+          type: Date
+        },
+        winningValue: {
+          type: Number
+        },
+        sectors: {
+          type: [String], 
+          default: [
+                    'Basic Materials',
+                    'Communication Services',
+                    'Consumer Cyclical',
+                    'Consumer Defensive',
+                    'Energy',
+                    'Financial Services',
+                    'Healthcare',
+                    'Industrials',
+                    'Real Estate',
+                    'Technology',
+                    'Utilities'
+        ]},
+        maxDailyTrades: {
+          type: Number, 
+          required: true, 
+          default: 10 
+        }, 
+        tradingFee: {
+          type: Number, 
+          required: true
+        }, 
+        minERating: {
+          type: Number, 
+          default: 0
+        },
+        minSRating: {
+          type: Number, 
+          default: 0
+        },
+        minGRating: {
+          type: Number, 
+          default: 0
+        },
+        accessCode: {
+          type: String, 
+          trim: true
+        },
+        leagueAdmin: {
+          type: Schema.Types.ObjectId, 
+          ref: 'UserData'
+        }, 
+        users: [{
+          type: Schema.Types.ObjectId, 
+          ref: 'UserData'
+        }], 
+        portfolios: [{
+          type: Schema.Types.ObjectId, 
+          ref: 'PortfolioData'
+        }]
     },
     { collection: 'leagues' },
-
     { timestamps: true },
   )
 

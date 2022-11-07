a0006eb1ce5fe4655635b473d3780ee0fe74fd35
Warren <warrenkavanagh1000@gmail.com>
2022-11-07 15:48:59 +0000

    When the user is logged in now the home greeting page will redirect them straight to the stock discovery page

--- src/screens/home/homeScreen.js
+++ src/screens/home/homeScreen.js
@@ -7,18 +7,36 @@
 /// Imports ///
 import { Container, Row, Col, Button } from 'react-bootstrap'
 import {useNavigate} from 'react-router-dom';
+import {useEffect} from 'react';
+
+
+/// Redux ///
+import {useSelector} from 'react-redux';
 
 function HomeScreen (){
-
+    /// Get the user state from redux 
+    const user = useSelector((state) => state.user)
+    const {userInfo} = user; 
+    
     const navigate = useNavigate(); 
 
-
+    /// When user clicks "Play" button 
     const navigateRegister = (e) => {
         e.preventDefault()
         navigate(`/register`)
-  }
+    } 
 
 
+    /// useEffect ///
+    //  Using the use effect hook to see if a user is already logged in by checking the userInfo state
+    //  If the userInfo state has something in then user already logged in 
+    useEffect (() => {
+        // Check if userInfo has something in it 
+        if(userInfo){
+            // Need to redirect to stock discovery  
+            navigate('/stockdiscovery');
+        }
+    },[userInfo,navigate])
 
     return(
         <Container>

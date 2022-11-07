cabca8e5121f864470bb95ed462e73477e81b535
Warren <warrenkavanagh1000@gmail.com>
2022-11-07 18:16:30 +0000

    Created the Game screen component and added it to the /game route

---
+++ src/screens/gameScreen/gameScreen.js
@@ -0,0 +1,13 @@
+/// Description:
+//      This is the screen which is displayed when the user clicks into "game" on the nav bar 
+
+function GameScreen(){
+    return(
+        <h1>
+            GameScreen
+        </h1>
+    )
+
+}
+
+export default GameScreen;

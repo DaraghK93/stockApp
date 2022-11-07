6ed44835f1a1286e67e920c2334d7b90c6bb09fe
Warren <warrenkavanagh1000@gmail.com>
2022-11-07 18:31:56 +0000

    Structured the page layout based on figma design

--- src/screens/gameScreen/gameScreen.js
+++ src/screens/gameScreen/gameScreen.js
@@ -1,12 +1,35 @@
 /// Description:
 //      This is the screen which is displayed when the user clicks into "game" on the nav bar 
 
+import { Container, Button, Row, Col} from "react-bootstrap"
+
+
 function GameScreen(){
     return(
-        <h1>
-            GameScreen
-        </h1>
-    )
+        <Container>
+            <Row>
+                <Col><h1>GameScreen</h1></Col>
+            </Row>
+            <Row>
+                <Col>My Games</Col>
+                <Col>Join a Game</Col>
+            </Row>
+            <Row>
+                <Col>Create a Game Component</Col>
+            </Row>
+            <Row>
+                <Col>
+                    Active Games 
+                </Col>
+            </Row>
+            <Row>
+                <Col>
+                    Inactive Games 
+                </Col>
+            </Row>
+        </Container>
+    
+        )
 
 }
 

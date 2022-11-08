10714cca1fb5962f4dca0a87023bd74e31deb6d2
Bearach Byrne <C15379616@mytudublin.ie>
2022-11-08 16:57:13 +0000

    Comments added to Python file

--- machineLearning/stockRecommendationSystem/modelGeneration/model_2/model_2.py
+++ machineLearning/stockRecommendationSystem/modelGeneration/model_2/model_2.py
@@ -1,18 +1,15 @@
+# Module imports
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
-import os
 
 from sklearn.metrics.pairwise import cosine_similarity
 from sklearn.decomposition import PCA
-
-# Capture similarity
 from sentence_transformers import SentenceTransformer
 
+# This function is used to read in the original dataset and save the computed vectors as a pickle file.
 def read_data():
     # Read in the s&p dataset
     stocks = pd.read_csv('machineLearning\stockRecommendationSystem\modelGeneration\model_1\sp500.csv')
-
     # Create array X of the all long business summaries
     X = np.array(stocks.longbusinesssummary)
 
@@ -22,24 +19,18 @@ def read_data():
     embeddings = model.encode(text_data, show_progress_bar=True)
     embed_data = embeddings
     X = np.array(embed_data)
-    n_comp = 5
-    pca = PCA(n_components=n_comp)
-    pca.fit(X)
-    pca_data = pd.DataFrame(pca.transform(X))
-    pca_data.head()
 
-    # Recommender function taken in modified form from:https://towardsdatascience.com/hands-on-content-based-recommender-system-using-python-1d643bf314e4
     cos_sim_data = pd.DataFrame(cosine_similarity(X))
-    # Write cosine similarity dataframe to .csv
-    cos_sim_data.to_csv("machineLearning\stockRecommendationSystem\modelGeneration\model_2\cosine_sim_data.csv", encoding='utf-8', index=False)
+    # Write cosine similarity dataframe to pickle file
     cos_sim_data.to_pickle('machineLearning\stockRecommendationSystem\modelGeneration\model_2\cosine_sim_data.pkl')
     stocks.to_csv("stock_data.csv", encoding='utf-8', index=False)
     return cos_sim_data, X, stocks
-
+    
+# Recommender function taken in modified form from:https://towardsdatascience.com/hands-on-content-based-recommender-system-using-python-1d643bf314e4
 def give_recommendations(index,  print_recommendation=False, print_recommendation_longbusinesssummary=False, print_sectors=False):
     # Read in stock data from the csv file
     stocks = pd.read_csv('stock_data.csv')
-    # Read in 
+    # Read in pickle file of vector data
     cos_sim_data = pd.read_pickle('machineLearning\stockRecommendationSystem\modelGeneration\model_2\cosine_sim_data.pkl')
 
     index_recomm = cos_sim_data.loc[index].sort_values(ascending=False).index.tolist()[1:21]
@@ -67,9 +58,11 @@ def give_recommendations(index,  print_recommendation=False, print_recommendatio
             print('The sector of the number %i recommended stock is this one:\n %s \n' % (
                 k, plot_q))
             k = k+1
-    return result
+    return result["Stocks"]
 
 
-recs = give_recommendations(0, print_recommendation=False, print_recommendation_longbusinesssummary=False, print_sectors=False)
-final_recs = recs["Stocks"]
-print(final_recs)
+# recs = give_recommendations(0, print_recommendation=False, print_recommendation_longbusinesssummary=False, print_sectors=False)
+# final_recs = recs["Stocks"]
+# print(final_recs)
+
+print(give_recommendations(1))

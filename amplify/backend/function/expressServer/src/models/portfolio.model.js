456db6a312da230add92f2e8a9f7e339e07a146e
Joey Corcoran <81182125+joeycor92@users.noreply.github.com>
2022-11-08 10:23:07 +0000

    Changes to portfolio model and controller

--- amplify/backend/function/expressServer/src/models/portfolio.model.js
+++ amplify/backend/function/expressServer/src/models/portfolio.model.js
@@ -4,15 +4,15 @@ const {Schema} = mongoose
 const PortfolioSchema = new mongoose.Schema(
   {
     portfolioName: { type: String, trim: true, required: true },
-    remainder: { type: String, trim: true},
-    holdings: [{stock: {type: Schema.Types.ObjectID, ref: 'stock'}, quantity: {type: Number}}],
+    remainder: { type: Number, trim: true},
+    holdings: [{stock: {type: Schema.Types.ObjectId, ref: 'stock'}, quantity: {type: Number}}],
     dateCreated: {type: String, trim: true},
     totalValue: {type: String, trim: true},
-    transactions: [{stock: {type: Schema.Types.ObjectID, ref: 'stock'}, units: {type:Number}, transactionType: {type: String}, date: {type: Date}}],
+    transactions: [{stock: {type: Schema.Types.ObjectId, ref: 'stock'}, units: {type:Number}, transactionType: {type: String}, date: {type: Date}}],
     valueHistory: [{date: {type: Date}, value: {type: Number}}],
     // value history would contain the totalValue, pushed to array at the end of each day 
-    leagues: [{type: Schema.Types.ObjectID, ref: 'leagues'}],
-    user: {type: Schema.Types.ObjectID, ref: 'UserData'}
+    leagues: [{type: Schema.Types.ObjectId, ref: 'leagues'}],
+    user: {type: Schema.Types.ObjectId, ref: 'UserData'}
   },
   { collection: 'portfolios' },
   // Timestamps used to create createdAt and updatedAt fields in the model that allows us to track when the entity was created/updated

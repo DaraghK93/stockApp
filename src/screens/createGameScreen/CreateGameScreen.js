61ea2b61f4b4064996579ef91e7a427f33ecb2ad
Warren <warrenkavanagh1000@gmail.com>
2022-11-09 15:05:44 +0000

    Added the buttons to the create game screens for next and previous

--- src/screens/createGameScreen/CreateGameScreen.js
+++ src/screens/createGameScreen/CreateGameScreen.js
@@ -1,6 +1,28 @@
+import { useState } from 'react'
+import { Container, Row, Col,Button} from "react-bootstrap"
+
 function CreateGameScreen(){
+    const [screen, setScreen] = useState(1)
+
+
+
     return(
-        <h1>Hello</h1>
+        <Container>
+            <Row className="containerContent">
+                <Col>
+                    <p>Hello</p>
+                </Col>
+            </Row>
+            <Row className="containerButtons" lg={2} md={2} xs={2}>
+                <Col className="prevNextCol">
+                    <Button>Back</Button>
+                </Col>
+                <Col className="prevNextCol">
+                    <Button>Next</Button>
+                </Col>            
+            </Row>
+
+        </Container>
     )
 }
 

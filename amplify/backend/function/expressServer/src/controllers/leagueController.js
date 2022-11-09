590082aef3d319cdbc4a94bdae4ff3eefa5924bd
Daragh Kneeshaw <daraghkneeshaw1@gmail.com>
2022-11-09 13:11:13 +0000

    fixed joinLeague and added comments

--- amplify/backend/function/expressServer/src/controllers/leagueController.js
+++ amplify/backend/function/expressServer/src/controllers/leagueController.js
@@ -1,6 +1,7 @@
 const League = require('../models/league.model')
 const User = require('../models/user.model')
 const leagueService = require('../services/leagueServices')
+const portfolioServices = require('../services/portfolioServices')
 // @desc create new league. a league is created and sent to the league-data
 // collection in the DB. the user's id is sent through the auth middleware,
 // this id is added to the users array as the first user in the league
@@ -21,7 +22,7 @@ const createLeague = async (req, res, next) => {
             WinningValue,
             maxDailyTrades,
             active,
-            // accessCode, 
+            finished
           } = req.body
 
     // check they have sent all fields
@@ -30,7 +31,9 @@ const createLeague = async (req, res, next) => {
       typeof startingBalance === 'undefined' ||
       typeof leagueType === 'undefined' ||
       typeof tradingFee === 'undefined' ||
-      typeof private === 'undefined'
+      typeof private === 'undefined' ||
+      typeof WinningValue === 'undefined' ||
+      typeof maxDailyTrades === 'undefined' 
     ) {
       // data is missing bad request
       res.status(400)
@@ -41,7 +44,7 @@ const createLeague = async (req, res, next) => {
         ),
       )
     } 
-    // const accessCode = Math.floor(Math.random() * 6) + 1
+
     // create new league object
     const league = {
         leagueName,
@@ -52,13 +55,12 @@ const createLeague = async (req, res, next) => {
         startDate,
         WinningValue,
         maxDailyTrades,
-        // accessCode,
         active,
+        finished,
         leagueAdmin: req.user.id,   // gets this from JWT
-        portfolios: [portfolios]
       };
    
-  
+  // call the service that generates the unique accessCode
    let newLeague = await leagueService.saveLeague(league)
 
     res.json({newLeague})
@@ -69,7 +71,9 @@ const createLeague = async (req, res, next) => {
       return next(err);
     }}
 
-
+// @desc gets all public leagues
+// @route get league/
+// @access private
 const getPublicLeagues = async (req, res, next) => {
     try {
       // just get the leagues that aren't private
@@ -93,15 +97,23 @@ const getPublicLeagues = async (req, res, next) => {
       return next(err);
     }
 }
+// @desc join league. user sends access code and if that exists it adds the
+// league Id to the user, userId to league, creates a portfoliio, adds the PortfolioId
+// to the league
+// @route post league/joinleague
+// @access private
 
 const joinLeaguebyCode = async (req, res, next) => {
   try {
+
       // get the accessCode
       const {accessCode} = req.body
+
       // get league object from db
       let league = await League.findOne({ accessCode })
-      // get _id from league
-      const {_id,users} = league 
+
+      // get _id, list of UserIds, Starting Balance, name from league
+      const {_id,users, startingBalance, leagueName} = league 
 
       // 404 for no such league
       if (!league) {
@@ -117,12 +129,26 @@ const joinLeaguebyCode = async (req, res, next) => {
         return next(new Error('User already in league'))
       }
 
+      // set portfolio object to send
+      const portfolio  = {
+        portfolioName: `${leagueName} Portfolio`,
+        startingBalance,
+        userId: req.user.id,
+        leagueId:_id,
+      }
+
+      // send portfolio object to services and return the portfolio if it is created
+      const leaguePortfolio = await portfolioServices.createPortfolio(portfolio)
+
+      // res.json({leaguePortfolio})
+
       // update the league object in db, find by accesscode and push user_id
-      await League.updateOne({accessCode},{$push: {users:req.user.id}})
+      league = await League.updateOne({accessCode},{$push: {users:req.user.id, portfolios: leaguePortfolio._id }})
 
       // push leagues to user
-      await User.updateOne({_id}, {$push: {leagues:_id}})
-      res.json({league})
+      const leagueUser = await User.updateOne({_id: req.user.id}, {$push: {leagues:_id}})
+      
+      res.json({leaguePortfolio,league, leagueUser})
 
 } catch (err) {
       console.error(err.message);

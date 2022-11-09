0e726eb67f9cd49d943a7fb348d8cdf37d1099dc
Warren <warrenkavanagh1000@gmail.com>
2022-11-09 19:03:03 +0000

    Created the gameNameTypeAndImage Component

---
+++ src/components/gameComponents/createGameScreenComponents/gameNameTypeAndImage.js
@@ -0,0 +1,11 @@
+function GameNameTypeImage(){
+    return(
+        <>
+            <h1>Game Details</h1>
+            <h2>Game Name</h2>
+            
+        </>
+    )
+}
+
+export default GameNameTypeImage

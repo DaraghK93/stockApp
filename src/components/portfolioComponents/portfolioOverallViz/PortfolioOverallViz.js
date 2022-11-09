244a60dc4c8c06edbf81ac7e4948e3e2dcdaeb92
Rebecca Kelly <bex.r.kelly@gmail.com>
2022-11-09 15:07:23 +0000

    changes made

--- src/components/portfolioComponents/portfolioOverallViz/PortfolioOverallViz.js
+++ src/components/portfolioComponents/portfolioOverallViz/PortfolioOverallViz.js
@@ -6,7 +6,28 @@ function PortfolioOverallViz() {
 
     const [data, setData] = useState();
 
-    const portfolioA = { valueHistory:[
+    // const portfolioA = { 
+    //     _id: 1,
+    //     portfolioName: "portfolioA",
+    //     valueHistory:[
+    //     { date: '01-10', value: 400 },
+    //     { date: '01-11', value: 700 },
+    //     { date: '01-12', value: 60 },
+    //     { date: '01-13', value: 700 },
+    //     { date: '01-14', value: 500 },
+    //     { date: '01-15', value: 400 },
+    //     { date: '01-16', value: 700 },
+    //     { date: '01-17', value: 60 },
+    //     { date: '01-18', value: 1000 },
+    //     { date: '01-19', value: 500 },
+    //     { date: '01-20', value: 400 },
+    //     { date: '01-21', value: 700 },
+    //     { date: '01-22', value: 60 },
+    //     { date: '01-23', value: 700 },
+    //     { date: '01-24', value: 500 }
+    // ]}
+
+    const portfolioA = [
         { date: '01-10', value: 400 },
         { date: '01-11', value: 700 },
         { date: '01-12', value: 60 },
@@ -22,7 +43,7 @@ function PortfolioOverallViz() {
         { date: '01-22', value: 60 },
         { date: '01-23', value: 700 },
         { date: '01-24', value: 500 }
-    ]}
+    ]
 
     const portfolioB = [
         { date: '01-10', value: 400 },

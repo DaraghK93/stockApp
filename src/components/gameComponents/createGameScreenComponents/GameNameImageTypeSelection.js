18ee1dbb90cf8cb7b72d93741383fa68ba9a126c
Warren <warrenkavanagh1000@gmail.com>
2022-11-10 18:31:18 +0000

    Fixed the sizing of the cards for game type

--- src/components/gameComponents/createGameScreenComponents/GameNameImageTypeSelection.js
+++ src/components/gameComponents/createGameScreenComponents/GameNameImageTypeSelection.js
@@ -1,17 +1,41 @@
 import {Dropdown,Card,Row,Col,Container,ButtonGroup,ToggleButton,Image} from "react-bootstrap"
+import AttachMoneyIcon from '@mui/icons-material/AttachMoney';
+import AccessTimeIcon from '@mui/icons-material/AccessTime';
 
 function GameNameImageTypeSelection(){
     return(
         <ButtonGroup>
-            <Row>
+            <Row md={2} sm={1} xs={1}>
             <Col>
-                    <ToggleButton>
-                        Duration
+                    <ToggleButton
+                        className="gameDetailsToggleButton"
+                        variant='outline-primary'
+                    >
+                        <Card className="gameDetailsGameTypeCard">
+                            <Card.Body>
+                                <Card.Title>Value Based</Card.Title>
+                                    <Card.Text>
+                                        Race to a goal value
+                                    </Card.Text>
+                                    <AttachMoneyIcon fontSize="large"/>
+                            </Card.Body>
+                        </Card>
                     </ToggleButton>
             </Col>
             <Col>
-                    <ToggleButton>
-                        Value
+                    <ToggleButton
+                        className="gameDetailsToggleButton"
+                        variant='outline-primary'
+                    >
+                        <Card className="gameDetailsGameTypeCard">
+                            <Card.Body>
+                                <Card.Title>Time Based</Card.Title>
+                                    <Card.Text>
+                                        Most profit at end wins
+                                    </Card.Text>
+                                    <AccessTimeIcon fontSize="large"/>
+                            </Card.Body>
+                        </Card>
                     </ToggleButton>
                 </Col>
             </Row>

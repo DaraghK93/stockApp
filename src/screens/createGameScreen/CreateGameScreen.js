fbc9cadaf963478e72a427a4bdb3c7628c6dd6d8
Warren <warrenkavanagh1000@gmail.com>
2022-11-10 12:43:19 +0000

    The pagination now works using only the card component the buttons have been removed from the create game screen as they are reused

--- src/screens/createGameScreen/CreateGameScreen.js
+++ src/screens/createGameScreen/CreateGameScreen.js
@@ -38,36 +38,46 @@ function CreateGameScreen(){
         <Container style={{"textAlign":"center","alignItems":"center"}}>
             <Row className="containerContent">
                 {screen === 1 ?
+                <Col>
                     <GameCreationOptionsCard screen={screen} setScreen={setScreen} gameType={gameType}>
-                        <h1>Hello</h1>
+                        <h1>This will be the name, game type and picture screen</h1>
                     </GameCreationOptionsCard>
+                </Col>
                 :screen === 2 ?
                 <Col>
-                    <h1>Duration</h1>
+                    <GameCreationOptionsCard screen={screen} setScreen={setScreen} gameType={gameType}>
+                        <h1>This will be the duration screen</h1>
+                    </GameCreationOptionsCard>
                 </Col>
                 :screen === 3 && gameType === "valueBased" ?
                 <Col>
-                    <h1>The Winning Value Screen</h1>
+                    <GameCreationOptionsCard screen={screen} setScreen={setScreen} gameType={gameType}>
+                        <h1>This will be the winning value screen</h1>
+                    </GameCreationOptionsCard>
                 </Col>
                 :(screen === 4 && gameType === "valueBased") || (screen === 3 && gameType === "timeBased")?
                     <Col>
-                        <h1>Stock Type Screen</h1>
+                        <GameCreationOptionsCard screen={screen} setScreen={setScreen} gameType={gameType}>
+                            <h1>This will be stock type screen</h1>
+                        </GameCreationOptionsCard>
                     </Col>
                 :(screen === 5 && gameType === "valueBased") || (screen === 4 && gameType === "timeBased")?
-                    <Col>
-                        <h1>ESG Restrictions</h1>
-                    </Col>
-                :(screen === 6 && gameType === "valueBased") || (screen === 5 && gameType === "timeBased")?
-                    <Col>
-                        <h1>Budget and Fees</h1>
-                    </Col>
-                :(screen === 7 && gameType === "valueBased") || (screen === 6 && gameType === "timeBased")?
                 <Col>
-                    <h1>Game Summary</h1>
+                    <GameCreationOptionsCard screen={screen} setScreen={setScreen} gameType={gameType}>
+                        <h1>This will be the ESG restrictions screen</h1>
+                    </GameCreationOptionsCard>
+                </Col>
+                :(screen === 6 && gameType === "valueBased") || (screen === 5 && gameType === "timeBased")?
+                <Col>
+                    <GameCreationOptionsCard screen={screen} setScreen={setScreen} gameType={gameType}>
+                        <h1>This will be the budget and fees screen</h1>
+                    </GameCreationOptionsCard>
                 </Col>
                 :
                 <Col>
-                    <h1>Hello</h1>
+                <GameCreationOptionsCard screen={screen} setScreen={setScreen} gameType={gameType}>
+                        <h1>This will be the Game Summary Screen</h1>
+                </GameCreationOptionsCard>
                 </Col>
                 }   
             </Row>

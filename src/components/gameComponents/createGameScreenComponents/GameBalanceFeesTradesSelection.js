9f3e76deb835025eb7434d2a3ffeb800b8a20b50
Warren <warrenkavanagh1000@gmail.com>
2022-11-16 12:36:08 +0000

    Added the suggestions for levels for each of the game fields for Blances and Fees screen

--- src/components/gameComponents/createGameScreenComponents/GameBalanceFeesTradesSelection.js
+++ src/components/gameComponents/createGameScreenComponents/GameBalanceFeesTradesSelection.js
@@ -14,7 +14,15 @@ function GameBalanceFeesTradeSelection({startingBalance, setStartingBalance, tra
             <Row>
                 <Col>
                     <Card.Text className="gameOptionsCardText">Starting Balance
-                    <InfoButtonModal title="Starting Balance" info={<p>The Starting Balance is the Dollar amount each player in the game will have to invest. Each player starts out with the same amount.</p>}/>
+                    <InfoButtonModal title="Starting Balance" info={
+                        <>
+                        <p>The Starting Balance is the Dollar amount each player in the game will have to invest.</p>
+                        <p>Each player starts out with the starting balance.</p>
+                        <p><span className="bolded">Relaxed ($300,000 - $1,000,000)</span>- Makes the game less realisitc but more relaxed game with freedom to invest</p>
+                        <p><span className="bolded">Medium ($25,000 - $300,000)</span>- Still gives user lot of freedom but need to be more cautious about some investments</p>
+                        <p><span className="bolded">Harsh ($1,000 - $25,000)</span>- More realisitic game, the user has less money to spend and needs to think about larger investments.</p>
+                        </>
+                   }/>
                     </Card.Text> 
                     
                 </Col>
@@ -39,6 +47,9 @@ function GameBalanceFeesTradeSelection({startingBalance, setStartingBalance, tra
                             <p>The Trading Fee is the dollar amount deducted from each trade made within the league. </p>
                             <p>The higher the trading fee the more risk associated with performing trades as they will cost more money to perform.</p>
                             <p>The lower the trading fee the more freedom the user has to perform trades without worrying about extra fees.</p>
+                            <p><span className="bolded">Relaxed ($1 - $20)</span>- Users wont be as penalised for making lots of trades</p>
+                            <p><span className="bolded">Medium ($20 - $150)</span>- Users penalised slighty more and may have to think more about trades.</p>
+                            <p><span className="bolded">Harsh ($150 - $300)</span>- Harsher penalty, the user will have to seriously think if they can make money on this trade factoring in the trade fee.</p>
                         </>
                         }/>
                     </Card.Text> 
@@ -61,8 +72,11 @@ function GameBalanceFeesTradeSelection({startingBalance, setStartingBalance, tra
                     <Card.Text className="gameOptionsCardText">Max Daily Trades
                     <InfoButtonModal title="Max Daily Trades" info={<>
                             <p>The Max Daily Trades sets a limit on the number of trades a user can make within a day.</p>
-                            <p>A smaller number set here will make the user have to use there trades more wisely.</p>
+                            <p>A smaller number set here will make the user have to use their trades more wisely.</p>
                             <p>A larger number set here will give players more freedom to trade.</p>
+                            <p><span className="bolded">Relaxed (50 - 100)</span>- Lot of freedom for the user to make lots of trades.</p>
+                            <p><span className="bolded">Medium (15 - 50)</span>- Bit tighter resticitions users will have to think more about how they use their daily limit.</p>
+                            <p><span className="bolded">Harsh (1 - 15)</span>- Very restrictive, a user will really need to think about how they can get the most out of their max daily trades.</p>
                             </>}/>
                     </Card.Text> 
                 </Col>

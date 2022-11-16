d588519aa2f0adc7acce2e029fc434dc7852c583
Warren <warrenkavanagh1000@gmail.com>
2022-11-16 16:45:07 +0000

    Added the title and the info modal to the winng value screen, still need to write explanation on the winning value

--- src/components/gameComponents/createGameScreenComponents/GameWinningValueSelection.js
+++ src/components/gameComponents/createGameScreenComponents/GameWinningValueSelection.js
@@ -5,12 +5,22 @@ import InfoButtonModal from "../../widgets/InfoButtonModal/InfoButtonModal"
 function GameWinningValueSelection(){
     return(
         <>
-            <Card.Title className="gameOptionsCardTitle">Balances and Fees</Card.Title>
+            <Card.Title className="gameOptionsCardTitle">Game Winning Value</Card.Title>
             <Row>
                 <Col>
-                    <Card.Text className="gameDetailsCardTitle">The </Card.Text>
+                    <Card.Text className="gameOptionsCardSubTitle">Winning Value
+                    <InfoButtonModal title="Winning Value" info={<>
+                            <p>The Max Daily Trades sets a limit on the number of trades you can make within a day.</p>
+                            <p>A smaller number set here will force you use to trade more wisely.</p>
+                            <p>A larger number set here will give you more freedom to trade.</p>
+                            <p><span className="bolded">Relaxed (50 - 100)</span>- A lot of freedom to make lots of trades.</p>
+                            <p><span className="bolded">Medium (15 - 50)</span>- Will force you to think more about how you use your daily trades.</p>
+                            <p><span className="bolded">Harsh (1 - 15)</span>- You will really need to think about how you can get the most out of your daily trades.</p>
+                            </>}/>
+                    </Card.Text>  
                 </Col>
             </Row>
+          
         </>
     )
 }

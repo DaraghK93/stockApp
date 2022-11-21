44c430a1644a4a7e8370fb5978dbaf31e3c77fd4
Warren <warrenkavanagh1000@gmail.com>
2022-11-21 17:31:01 +0000

    added the screen selection button to the GameSectorsSelection component

--- src/components/gameComponents/createGameScreenComponents/GameSectorsSelection.js
+++ src/components/gameComponents/createGameScreenComponents/GameSectorsSelection.js
@@ -1,6 +1,24 @@
-function GameSectorsSelection(){
+import {Card,Row,Col} from "react-bootstrap"
+import ScreenSelectionRadioButton from "../gameScreenComponents/screenSelectionRadioButton/screenSelectionRadioButton"
+import { useState} from 'react'
+function GameSectorsSelection({}){
+    const [screen, setScreen] = useState('1')
+
+    /// The choices for the screens, used for buttons at top of screen 
+    var screenChocies = [
+        {name: 'All Sectors', value:'1'},
+        {name: 'Select Sectors', value: '2'}
+    ]
+    
+
+
     return(
-        <h1>Hello</h1>
+        <>
+            <Card.Title  className="gameOptionsCardTitle">Stock Sectors</Card.Title>
+            <Row>
+                <ScreenSelectionRadioButton choices={screenChocies} state={screen} setter={setScreen}/>
+            </Row>
+        </>
     )
 }
 

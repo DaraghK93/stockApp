3fbfe4f1ddaf8fe1fa1a516544cc05ec72c1b9d1
Caolán Power <d21125982@mytudublin.ie>
2022-11-21 13:44:06 +0000

    changeuserdetails backend functional except for avatar

--- amplify/backend/function/expressServer/src/controllers/authController.js
+++ amplify/backend/function/expressServer/src/controllers/authController.js
@@ -15,6 +15,59 @@ const changeUserDetails = async (req, res, next) => {
         new Error('Current password required to change any user details')
       );
     }
+    // check if old password is correct
+    const user = await User.findById(req.user.id);
+    // compare old password with currpassword
+    const isMatch = await bcrypt.compare(currPassword, user.password);
+
+    if (!isMatch) {
+      res.status(401);
+      res.errormessage = 'Current password incorrect';
+      return next(new Error('Current Password incorrect'));
+    } else {
+      newPassword = req.body.newPassword;
+      if (newPassword) {
+        // Checks for minimum password length, if contains a lower case character (a-z), an upper case character (A-Z), and a number (0-9)
+        if (
+          newPassword.length < 8 ||
+          !/[a-z]/.test(newPassword) ||
+          !/[A-Z]/.test(newPassword) ||
+          !/[0-9]/.test(newPassword)
+        ) {
+          res.status(400);
+          res.errormessage =
+            'Password must be at least 8 characters long, contain at least one lower case English character (a-z), at least one upper case English character (A-Z) and at least one number (0-9)!';
+          return next(
+            new Error(
+              'Password must be at least 8 characters long, contain at least one lower case English character (a-z), at least one upper case English character (A-Z) and at least one number (0-9)!'
+            )
+          );
+        }
+
+        /// Hash the new password ///
+        let salt = await bcrypt.genSalt(10);
+        newPassword = await bcrypt.hash(newPassword, salt);
+
+        user.password = newPassword;
+      }
+      // checks for username update
+      const newUsername = req.body.username;
+      if (newUsername) {
+        if (newUsername.length < 3) {
+          res.status(400);
+          res.errormessage = 'Username length must be at least 3 characters';
+          return next(
+            new Error('Username length must be at least 3 characters')
+          );
+        }
+        // assign username to user after all checks
+        user.username = newUsername;
+      }
+
+      await user.save();
+      res.status(200);
+      res.json({ user });
+    }
   } catch (error) {
     res.status(500);
     res.errormessage = 'Server error in changing user details';
--- amplify/backend/function/expressServer/src/routes/authRoutes.js
+++ amplify/backend/function/expressServer/src/routes/authRoutes.js
@@ -3,6 +3,7 @@
 const express = require('express');
 const router = express.Router();
 const { protectedRoute } = require('../middleware/authMiddleware');
+const { changeUserDetails } = require('../controllers/authController');
 
 router.post('/changeUserDetails', protectedRoute, changeUserDetails);
 

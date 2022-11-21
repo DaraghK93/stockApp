1b0e060d82449fddcbb2cabb868f43dedf4a0b69
Caolán Power <d21125982@mytudublin.ie>
2022-11-21 15:57:44 +0000

    Front-end calling backend

--- src/screens/userSettingsScreen/UserSettingsPage.js
+++ src/screens/userSettingsScreen/UserSettingsPage.js
@@ -18,21 +18,29 @@ import FormContainer from '../../components/layout/FormContainer/FormContainer';
 import { API } from 'aws-amplify';
 import { APIName } from '../../constants/APIConstants';
 import { useEffect } from 'react';
+import { useSelector } from 'react-redux';
 
 function UserSettingsPage() {
   // constant password holds the value of the input password
   const [password, setPassword] = useState('');
+  const [currPasswordEnteredError, setCurrPasswordEnteredError] =
+    useState(false);
+  // new passwords
   const [newPassword, setNewPassword] = useState('');
   const [confirmNewPassword, setConfirmNewPassword] = useState('');
   const [passwordErrorMessage, setPasswordErrorMessage] = useState(null);
   // add state for passwordChange
-  const [passwordChanged, setPasswordChanged] = useState(false);
   const [loading, setLoading] = useState(false);
 
   const [gameImage, setGameImage] = useState('');
   const [passwordShown, setPasswordShown] = useState(false);
 
   const [username, setUsername] = useState('');
+
+  //Redux
+  const user = useSelector((state) => state.user);
+  const { userInfo } = user;
+  const userToken = userInfo.token;
 
   var images = [
     '/stock_photo_1.jpg',
@@ -47,58 +55,75 @@ function UserSettingsPage() {
 
   function handleSubmit(event) {
     event.preventDefault();
-    // Check whether password and confirm password are the same. More messages will need to be added.
-    if (newPassword !== confirmNewPassword) {
-      setPasswordErrorMessage(
-        'Password and Confirm Password must be the same!'
-      );
-    }
-    // Checks if the password is less than 8 characters. Gives a popup warning if it is.
-    else if (newPassword.length < 8) {
-      setPasswordErrorMessage('Password must be at least 8 characters!');
-    }
-    // Checks if the password contains a lower case character (a-z).
-    else if (!/[a-z]/.test(newPassword)) {
-      setPasswordErrorMessage(
-        'Password must contain at least one lower case English character (a-z)!'
-      );
-    }
-    // Checks if the password contains an upper case character (A-Z).
-    else if (!/[A-Z]/.test(newPassword)) {
-      setPasswordErrorMessage(
-        'Password must contain at least one upper case English character (A-Z)!'
-      );
-    }
-    // Checks if the password contains number (0-9).
-    else if (!/[0-9]/.test(newPassword)) {
-      setPasswordErrorMessage(
-        'Password must contain at least one number (0-9)!'
-      );
+    if (password) {
+      // determine if password is to be changed
+      if (newPassword) {
+        // Check whether password and confirm password are the same. More messages will need to be added.
+        if (newPassword !== confirmNewPassword) {
+          setPasswordErrorMessage(
+            'Password and Confirm Password must be the same!'
+          );
+        }
+        // Checks if the password is less than 8 characters. Gives a popup warning if it is.
+        else if (newPassword.length < 8) {
+          setPasswordErrorMessage('Password must be at least 8 characters!');
+        }
+        // Checks if the password contains a lower case character (a-z).
+        else if (!/[a-z]/.test(newPassword)) {
+          setPasswordErrorMessage(
+            'Password must contain at least one lower case English character (a-z)!'
+          );
+        }
+        // Checks if the password contains an upper case character (A-Z).
+        else if (!/[A-Z]/.test(newPassword)) {
+          setPasswordErrorMessage(
+            'Password must contain at least one upper case English character (A-Z)!'
+          );
+        }
+        // Checks if the password contains number (0-9).
+        else if (!/[0-9]/.test(newPassword)) {
+          setPasswordErrorMessage(
+            'Password must contain at least one number (0-9)!'
+          );
+        } else {
+          setPasswordErrorMessage('');
+        }
+      }
+      setCurrPasswordEnteredError(false);
+      if (passwordErrorMessage === null) {
+        changeUserDetails();
+      }
     } else {
-      changeUserDetails();
+      setCurrPasswordEnteredError(true);
     }
-    console.log(gameImage);
   }
 
   const changeUserDetails = async () => {
     try {
+      console.log('hi');
+
+      let body = {
+        password: password,
+        username: username ? username : undefined,
+        newPassword: newPassword ? newPassword : undefined,
+      };
+
+      setCurrPasswordEnteredError(false);
       setLoading(true);
       // Configure the HTTP request
-      let path = `/api/auth//`;
+      let path = `/api/auth/changeuserdetails`;
       let requestConfig = {
-        body: {
-          password: password,
-        },
+        body,
+        headers: { 'x-auth-token': userToken },
       };
       // Sent the request to backend
       const data = await API.post(APIName, path, requestConfig);
-      setPasswordChanged(true);
       setLoading(false);
       console.log('data = ' + data);
       console.log(data.errormessage);
     } catch (error) {
       setLoading(false);
-      setPasswordErrorMessage(error.response.data.errormessage);
+      //setPasswordErrorMessage(error.response.data.errormessage);
     }
   };
 
@@ -120,21 +145,24 @@ function UserSettingsPage() {
   return (
     <>
       <FormContainer>
-        {passwordErrorMessage && (
-          <MessageAlert variant='danger'>{passwordErrorMessage}</MessageAlert>
-        )}
-        {passwordChanged && (
-          <MessageAlert variant='success'>
-            {
-              <a href='/login' style={{ color: '#006195' }}>
-                Success Login here!
-              </a>
-            }
+        <h1> Settings</h1>
+        {currPasswordEnteredError && (
+          <MessageAlert variant='danger'>
+            {'Current password is required'}
           </MessageAlert>
         )}
         {loading && <LoadingSpinner />}
-        <h1> Settings</h1>
-        <p>Choose avatar</p>
+        <Form.Group className='py-2' controlId='oldPassword'>
+          <Form.Label>Current Password* - required</Form.Label>
+          <Form.Control
+            type='password'
+            placeholder='Enter current password'
+            value={password}
+            onChange={(event) => setPassword(event.target.value)}
+            required
+          />
+        </Form.Group>
+        <p>Choose new avatar</p>
         <Row className='py-3' md={4} sm={2} xs={2}>
           {images.map((image, idx) => (
             <Col key={`col-image-${image}`}>
@@ -157,33 +185,24 @@ function UserSettingsPage() {
         </Row>
         <Form onSubmit={handleSubmit}>
           <Form.Group className='py-2' controlId='username'>
-            <Form.Label>Username</Form.Label>
+            <Form.Label>New Username</Form.Label>
             <Form.Control
               type='text'
-              placeholder='Enter Username'
+              placeholder='Enter New Username'
               value={username}
               onChange={(event) => setUsername(event.target.value)}
-              required
             />
           </Form.Group>
-          <Form.Group className='py-2' controlId='oldPassword'>
-            <Form.Label>Old Password</Form.Label>
-            <Form.Control
-              type='password'
-              placeholder='Enter current password'
-              value={password}
-              onChange={(event) => setPassword(event.target.value)}
-              required
-            />
-          </Form.Group>
+          {passwordErrorMessage && (
+            <MessageAlert variant='danger'>{passwordErrorMessage}</MessageAlert>
+          )}
           <Form.Group className='py-2' controlId='newPassword'>
-            <Form.Label>Enter New Password</Form.Label>
+            <Form.Label>New Password</Form.Label>
             <Form.Control
               type={passwordShown ? 'text' : 'password'}
               placeholder='Enter New Password'
               value={newPassword}
               onChange={(event) => setNewPassword(event.target.value)}
-              required
             />
           </Form.Group>
           <Form.Group className='py-2' controlId='confirmNewPassword'>
@@ -193,7 +212,6 @@ function UserSettingsPage() {
               placeholder='Confirm New Password'
               value={confirmNewPassword}
               onChange={(event) => setConfirmNewPassword(event.target.value)}
-              required
             />
           </Form.Group>
           <Button onClick={togglePassword}>Show password</Button>

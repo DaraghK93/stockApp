107803b139e99ceb7ee00daf352b93dafc710f66
Joey Corcoran <81182125+joeycor92@users.noreply.github.com>
2022-11-22 20:03:18 +0000

    Make it so daily stocks come through

--- amplify/backend/function/expressServer/src/services/stockRoutesServices.js
+++ amplify/backend/function/expressServer/src/services/stockRoutesServices.js
@@ -6,6 +6,7 @@ var moment = require('moment')
 
 function getStockPriceData (stocks) {
     // this function allows the user to get the weekly, monthly and yearly data for a stock
+    const daily_prices = stocks[0].prices
     const weekly_prices = stocks[0].prices
     const monthly_prices = stocks[0].prices
     const yearly_prices = stocks[0].prices
@@ -19,7 +20,7 @@ function getStockPriceData (stocks) {
     // https://bobbyhadz.com/blog/javascript-get-all-dates-between-two-dates#:~:text=To%20get%20all%20of%20the%20dates%20between%202%20dates%3A&text=Copied!,(date))%3B%20date. 
     function getDatesInRange(startDate, endDate) {
         // the below function will create an array of dates from today back one month
-        const date = new Date(startDate.getTime());
+        const date = new Date(startDate.getTime())
     
         const dates = []
         const dates_no_year = []
@@ -37,6 +38,20 @@ function getStockPriceData (stocks) {
         }
         return [dates, dates_no_year];
     }
+    var newArray = []
+    const startTime = moment()
+    const remainder = 20 - (startTime.minute() % 20)
+    const datestart = moment(startTime).add(remainder, "minutes")
+    const oneDayTimes = timeOneDay(datestart)
+    function timeOneDay(datestart) {
+        var formattedTime;
+        for (i = 0; i < 24*3 + 1; i++) { //fill in all of the hours
+          formattedTime = (moment(datestart).subtract(i*20, "minutes")).format("YYYY-MM-DD[T]HH:mm");
+          newArray.push(formattedTime); //add to beginning of array
+        } //do this for all 24 hours
+        return newArray
+      }
+
     // Date() gives us the current date, Date(end_Date) interprets the 
     // input date and creates a date that can be used in the function
     const weekStart = new Date(start_Date_week)
@@ -56,15 +71,21 @@ function getStockPriceData (stocks) {
     dateRangeYearNoYear = getDatesInRange(yearStart, today)[1]
 
     // create an object for the prices (similar to python dictionary)
+    const prices_day = []
     const prices_week = []
     const prices_month = []
     const prices_year = []
-
+    // ***One Day***
+    // loop through the date range list and extract the data
+    for (var i = 0; i < oneDayTimes.length; i++) { 
+        // the format of the data keys is "YYYY-MM-DDT20:00"
+        if(typeof(daily_prices[oneDayTimes[i]]) != 'undefined'){     
+        prices_day.push({"date": oneDayTimes[i], "price": daily_prices[oneDayTimes[i]]["Close"] })}}
+    console.log(prices_day)
     // ***One Week***
     // loop through the date range list and extract the data
     for (var i = 0; i < dateRangeWeek.length; i++) { 
         // the format of the data keys is "YYYY-MM-DDT20:00"
-        // console.log(monthly_prices[date_range[i]+"T20:00"])
         if(typeof(weekly_prices[dateRangeWeek[i]+"T20:00"]) != 'undefined'){     
         prices_week.push({"date": dateRangeWeekNoYear[i], "price": weekly_prices[dateRangeWeek[i]+"T20:00"]["Close"] })}}
         
@@ -72,7 +93,6 @@ function getStockPriceData (stocks) {
     // loop through the date range list and extract the data
     for (var i = 0; i < dateRangeMonth.length; i++) { 
         // the format of the data keys is "YYYY-MM-DDT20:00"
-        // console.log(monthly_prices[date_range[i]+"T20:00"])
         if(typeof(monthly_prices[dateRangeMonth[i]+"T20:00"]) != 'undefined'){
         
         prices_month.push({"date": dateRangeMonthNoYear[i], "price": monthly_prices[dateRangeMonth[i]+"T20:00"]["Close"] })}}
@@ -81,11 +101,10 @@ function getStockPriceData (stocks) {
     // loop through the date range list and extract the data
     for (var i = 0; i < dateRangeYear.length; i++) { 
         // the format of the data keys is "YYYY-MM-DDT20:00"
-        // console.log(monthly_prices[date_range[i]+"T20:00"])
         if(typeof(yearly_prices[dateRangeYear[i]+"T20:00"]) != 'undefined'){
         prices_year.push({"date": dateRangeYearNoYear[i], "price": yearly_prices[dateRangeYear[i]+"T20:00"]["Close"] })}}
     
-    return [prices_week, prices_month,prices_year]
+    return [prices_week, prices_month,prices_year, prices_day]
 
 };
 

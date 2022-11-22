b18015c3fed3d082e372dd4fa9848233c1e271ca
Warren <warrenkavanagh1000@gmail.com>
2022-11-22 19:15:30 +0000

    Added the number of stock avaiable to trade in each mode, the info modal for ESG and a short description on the card for this page

--- src/components/gameComponents/createGameScreenComponents/GameESGRestrrictionsSelection.js
+++ src/components/gameComponents/createGameScreenComponents/GameESGRestrrictionsSelection.js
@@ -5,6 +5,7 @@ import EnvironmentalRating from "../../stockVisualisationComponents/ESGRatingSli
 import SocialRating from "../../stockVisualisationComponents/ESGRatingSliders/SocialRating";
 import GovernanceRating from "../../stockVisualisationComponents/ESGRatingSliders/GovernanceRating";
 import EvilRating from "../../stockVisualisationComponents/ESGRatingSliders/EvilRating";
+import InfoButtonModal from "../../widgets/InfoButtonModal/InfoButtonModal";
 
 function GameESGRestrictionsSelection(){
     const [ESGGameType,setESGGameType] = useState()
@@ -14,6 +15,7 @@ function GameESGRestrictionsSelection(){
     const ESGRating = {
        "No Restrictions" : {
             "description":"For people with No Conscious",
+            "stocks":490,
             "eRating":0,
             "sRating":0,
             "gRating":5
@@ -21,18 +23,21 @@ function GameESGRestrictionsSelection(){
         
       "Environment" : {
             "description": "For the Tree Huggers",
+            "stocks":408,
             "eRating":4,
             "sRating":0,
             "gRating":0
         },
         "Social":{
             "description":"For Human Rights Activists",
+            "stocks":412,
             "eRating":0,
             "sRating":2.5,
             "gRating":0
         },
        "Governance":{
             "description": "For the Ethical Investors",
+            "stocks":338,
             "eRating":0,
             "sRating":0,
             "gRating":3.5
@@ -45,10 +50,28 @@ function GameESGRestrictionsSelection(){
 
     return(
         <>
-            <Card.Title className="gameOptionsCardTitle">ESG Restrictions</Card.Title>
+            <Card.Title className="gameOptionsCardTitle">ESG Restrictions
+                <InfoButtonModal title="What Is an ESG Rating?" 
+                info={
+                <div>
+                    <h4>What does an ESG rating mean?</h4>
+                        <p>
+                            A good ESG rating means a company is managing its environment, social, and governance risks well relative to its peers.
+                        </p>
+                        <p>
+                            A poor ESG rating is the opposite -- the company has relatively higher unmanaged exposure to ESG risks.
+                        </p>
+                        <p>
+                            You can read more about ESG ratings in this article! Just click  <a href="https://www.fool.com/investing/stock-market/types-of-stocks/esg-investing/esg-rating/" target="_blank" rel="noopener noreferrer">here</a> to be redirected.
+                        </p>
+                              
+                </div>}/>
+            
+            </Card.Title>
             <Row>
                 <Col>
-                        
+                    <Card.Text className="gameOptionsCardText">The restrictions below will set a minimum ESG rating a company must meet</Card.Text>    
+                    <Card.Text className="gameOptionsCardSubTitle">Please Select One Game Mode</Card.Text>
                 </Col>
             </Row>
              <Row className="py-3" md={2} sm={1} xs={1}>
@@ -73,6 +96,9 @@ function GameESGRestrictionsSelection(){
                         <Card.Body>
                             <Card.Text className="black">
                                 {ESGRating[value].description}
+                            </Card.Text>
+                            <Card.Text className="black">
+                                {ESGRating[value].stocks} stocks to choose from
                             </Card.Text>
                             { value === "Environment" ?
                                 <EnvironmentalRating erating={ESGRating[value].eRating} />

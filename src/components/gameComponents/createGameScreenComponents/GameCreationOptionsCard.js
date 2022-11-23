b5a4b6e3106cc7ef40fff8d04b98e062840fb731
Warren <warrenkavanagh1000@gmail.com>
2022-11-23 18:20:00 +0000

    Created the function createGame in the GameCreationOptionsCard, called when the finish button is clicked, have the outline of the request to be sent

--- src/components/gameComponents/createGameScreenComponents/GameCreationOptionsCard.js
+++ src/components/gameComponents/createGameScreenComponents/GameCreationOptionsCard.js
@@ -2,15 +2,69 @@
 //      This coponent is used for the game creation screen
 //      It is used to display the options for creating a game along with the pagination
 import {Card,Row,Col,Button} from "react-bootstrap"
+import { APIName } from '../../../constants/APIConstants'
+import { useState } from 'react';
+import { API } from "aws-amplify";
+import {useSelector} from 'react-redux';
+
 
 /// GameCreationOptionsCard ///
-//  poprs:
+//  prprs:
 //      children         - The children of the component https://reactjs.org/docs/composition-vs-inheritance.html
 //      setScreen        - Setter to set createGameScreen state
 //      screen           - The screen state set by setScreen
 //      gameType         - The type of game, depedning upon game type there will  be extra screen for valueBased game 
 //      disableNextStep  - This is boolean, true then cant go onto next step, false then you can go onto next step 
-function GameCreationOptionsCard({children, setScreen, screen, gameType, disableNextStep}){
+function GameCreationOptionsCard({children, setScreen, screen, disableNextStep, 
+                                gameName, gameType, gameImage, gameStartDate, gameEndDate, 
+                                startingBalance, tradingFee, maxTradesPerDay, gameWinningValue,  
+                                stockTypes, minEnvironmentRating,  minSocialRating, minGovernanceRating}){
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState("");
+
+    //Redux
+    const user = useSelector((state) => state.user)
+    const {userInfo} = user
+    const userToken = userInfo.token
+
+    const createGame = async () => {
+        try{
+            setLoading(true)
+            let path = '/api/league/createleague'
+            // token in header
+            // Set the common fields in the body 
+            let myInit = {
+                    headers : {"x-auth-token": userToken},       
+                    body:{
+                        "leagueName":gameName ,
+                        "leagueType":gameType,
+                        "startingBalance": startingBalance,
+                        "maxDailyTrades": maxTradesPerDay,
+                        "tradingFee": tradingFee,
+                        "startDate": gameStartDate,
+                        "image": gameImage,
+                        "sectors": stockTypes,
+                        "minERating": minEnvironmentRating,
+                        "mingSRating": minSocialRating,
+                        "mingGRating": minGovernanceRating
+                    }
+            }
+            /// Set unique fields to the game type 
+            if (gameType === "valueBased"){
+                myInit.body.winningValue = gameWinningValue
+            }else if(gameType === "valueBased"){
+                myInit.body.endDate = gameEndDate
+            }
+            /// Send the request 
+            const res = await API.get(APIName, path, myInit)
+            
+            setLoading(false)
+        }catch(error){
+            setError(error.response.data.errormessage)
+            setLoading(false)
+        }
+    }
+
     return(
         <Card className="my-5">
             <Card.Body>
@@ -31,9 +85,7 @@ function GameCreationOptionsCard({children, setScreen, screen, gameType, disable
                             <Button
                                 className="prevNextButtons"
                                 disabled={disableNextStep}
-                                onClick={() => {
-                                    console.log("Im done this will eventually be redirect")
-                                }}
+                                onClick={createGame}
                             >Finish</Button>
                         </Col>
                         :

6850aa45e4023627305cc6e3271b1d9498819502
Rebecca Kelly <bex.r.kelly@gmail.com>
2022-11-24 18:23:30 +0000

    updated nav

--- src/components/gameComponents/individualGameScreenComponents/gameNavigation/GameNavBar.js
+++ src/components/gameComponents/individualGameScreenComponents/gameNavigation/GameNavBar.js
@@ -3,13 +3,15 @@ import { Navbar, Container, Nav } from 'react-bootstrap'
 function GameNavBar({ disPlayScreen }) {
     return (
         <Navbar expand="lg" className="navbar navbar-expand" variant="dark">
+      
             <Container>
                 <Nav className="me-auto" >
                     {/* <Nav.Link style={{ color: 'white' }}>Activity</Nav.Link> */}
                     <Nav.Link id={"1"} style={{ color: 'white' }} onClick={disPlayScreen}>Rules</Nav.Link>
                     <Nav.Link id={"2"} style={{ color: 'white' }} onClick={disPlayScreen}>Stocks</Nav.Link>
                     <Nav.Link id={"3"} style={{ color: 'white' }} onClick={disPlayScreen}>Portfolio</Nav.Link>
-                    <Nav.Link id={"4"} style={{ color: 'white' }} onClick={disPlayScreen}>Settings</Nav.Link>
+                    <Nav.Link id={"4"} style={{ color: 'white' }} onClick={disPlayScreen}>Leaderboard</Nav.Link>
+
                 </Nav>
             </Container>
         </Navbar>
--- src/screens/individualGameScreen/IndividualGameScreen.js
+++ src/screens/individualGameScreen/IndividualGameScreen.js
@@ -4,28 +4,28 @@ import PortfolioPage from "../portfolio/portfolio";
 import { Container } from 'react-bootstrap'
 function IndividualGameScreen() {
 
+
     const [screen, setScreen] = useState("")
 
     const disPlayScreen = (e) => {
         if (e.target.id === "1") {
-            setScreen(<br></br>)
+            setScreen(<><br></br><h2>This is rules screen</h2></>)
         }
         else if (e.target.id === "2") {
-            setScreen(<br></br>)
+            setScreen(<><br></br><h2>This is stocks screen</h2></>)
         }
         else if (e.target.id === "3") {
             setScreen(<PortfolioPage />)
         }
         else if (e.target.id === "4") {
-            setScreen(<br></br>)
+            setScreen(<><br></br><h2>This is Leaderboard screen</h2></>)
         }
     }
 
     return (
         <>
-            <Container>
-                <h1>Game image</h1>
-            </Container>
+            <img style={{ width: "100%" }} src="/stock_photo_1.jpg"></img>
+
             <GameNavBar disPlayScreen={disPlayScreen} />
             <Container>
                 <h1>Timeline</h1>

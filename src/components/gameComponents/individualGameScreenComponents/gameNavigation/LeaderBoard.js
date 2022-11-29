81345158803b3777fbb0f3ba7164f1d2dc134871
Rebecca Kelly <bex.r.kelly@gmail.com>
2022-11-29 20:55:42 +0000

    finsihing off leaderboard

--- src/components/gameComponents/individualGameScreenComponents/gameNavigation/LeaderBoard.js
+++ src/components/gameComponents/individualGameScreenComponents/gameNavigation/LeaderBoard.js
@@ -1,7 +1,7 @@
-import { ContactEmergency } from "@mui/icons-material";
-import { useState, Fragment } from "react"
+import { Fragment } from "react"
 import { Image, Row, Col, Table, Container } from 'react-bootstrap';
-
+import KeyboardDoubleArrowUpIcon from '@mui/icons-material/KeyboardDoubleArrowUp';
+// import KeyboardDoubleArrowDownIcon from '@mui/icons-material/KeyboardDoubleArrowDown';
 
 function LeaderBoard() {
 
@@ -53,103 +53,81 @@ function LeaderBoard() {
     const Top3 = leaderBoardData.slice(0, 3)
     const RestOfLeaderBoard = leaderBoardData.splice(3)
     const columns = [
-        { label: "position", accessor: "position", sortable: false },
-        { label: "avatar", accessor: "avatar", sortable: false },
-        { label: "username", accessor: "username", sortable: true, sortbyOrder: "desc" },
-        { label: "amount", accessor: "amount", sortable: false },
-        { label: "+-", accessor: "dailyChange", sortable: false },
+        { label: "Rank", accessor: "position", sortable: false },
+        { label: "Pic", accessor: "avatar", sortable: false },
+        { label: "User", accessor: "username", sortable: true, sortbyOrder: "desc" },
+        { label: "Total", accessor: "amount", sortable: false },
+        { label: "Change", accessor: "dailyChange", sortable: false },
     ];
+
 
     return (
 
         <>
-            <div style={{ backgroundColor: "#02478D", color: "#D6EBFF" }}>
-                <Container >
+            <div className="leaderBoardStyle">
+                <Container style={{ width: "95%" }} >
                     <center>
                         <br></br><br></br>
                         <Col s={6} md={6} lg={6} xl={6}>
 
-                            <Row xs={3}
-                                style={{ color: "#D6EBFF" }}
-                            >
-                                <Col xs={4} style={{ textAlign: "center" }}>
-                                    <Image src={"/avatar.png"} style={{ maxWidth:"10rem", width: "100%", marginTop: "5rem", borderRadius: "50%", border: "2px solid #D6EBFF" }}></Image>
-                                    <p><strong><span style={{ color: "#FF7F11" }}>2nd place</span></strong>   <br></br>
+                            <Row xs={3} style={{ textAlign: "center" }}>
+                                <Col xs={4}>
+                                    <Image src={"/avatar.png"} className="top3ImgStyle secondplace" />
+                                    <p><strong><span className="rankingText" >2nd place</span></strong>   <br></br>
                                         @{Top3[1].userName}
                                         <br></br>
                                         ${Top3[1].amount}<br></br>
-                                        (+{Top3[1].dailyChange})
+                                        +${Top3[1].dailyChange}
                                     </p>
                                 </Col>
-                                <Col xs={4} style={{ textAlign: "center" }}>
-                                    <Image src={"/crown.png"} style={{ width: "50%" }}></Image><div style={{ height: "5px" }}></div>
-                                    <Image src={"/avatar.png"} style={{ maxWidth:"10rem", width: "100%", borderRadius: "50%", marginTop: 0, border: "2px solid #D6EBFF" }}></Image>
-                                    <p><strong><span style={{ color: "#FF7F11" }}>1st place</span></strong>  <br></br>
+                                <Col xs={4}>
+                                    <Image src={"/crown.png"} style={{ width: "50%" }} /><div style={{ height: "5px" }} />
+                                    <Image src={"/avatar.png"} className="top3ImgStyle firstplace" />
+                                    <p><strong><span className="rankingText">1st place</span></strong>  <br></br>
                                         @{Top3[0].userName}
                                         <br></br>
                                         ${Top3[0].amount}<br></br>
-                                        (+{Top3[0].dailyChange})
+                                        +${Top3[0].dailyChange}
                                     </p>
                                 </Col>
-                                <Col xs={4} style={{ textAlign: "center" }}>
-                                    <Image src={"/avatar.png"} style={{ maxWidth:"10rem", width: "100%", marginTop: "6rem", borderRadius: "50%", border: "2px solid #D6EBFF" }}></Image>
-                                    <p><strong><span style={{ color: "#FF7F11" }}>3rd place</span></strong><br></br>
+                                <Col xs={4}>
+                                    <Image src={"/avatar.png"} className="top3ImgStyle thirdplace" />
+                                    <p><strong><span className="rankingText">3rd place</span></strong><br></br>
                                         @{Top3[2].userName}
                                         <br></br>
                                         ${Top3[2].amount}<br></br>
-                                        (+{Top3[2].dailyChange})
+                                        +${Top3[2].dailyChange}
                                     </p>
                                 </Col>
                                 <br></br><br></br>
                             </Row>
-
-
                             <Row>
                                 <Col>
                                     <Table style={{ borderCollapse: "collapse" }}>
-                                        {/* <thead>
-                            <tr key="cols">
-                                {columns.map(({ label, accessor }) => {
-                                    return <th key={accessor}>{label}</th>;
-                                })}
-                            </tr>
-                        </thead> */}
+                                        <thead>
+                                            <tr key="cols" className="leaderBoardHeaderStyle">
+                                                {columns.map(({ label, accessor }) => {
+                                                    return <th key={accessor}>{label}</th>;
+                                                })}
+                                            </tr>
+                                        </thead>
                                         <tbody style={{ backgroundColor: "none" }}>
                                             {RestOfLeaderBoard.map((item) => (
                                                 (<Fragment key={`${item.userName}-fragment`}>
-                                                    <tr key={item.userName}
-                                                        style={{
-                                                            backgroundColor: "#0080FF", verticalAlign: "middle",
-                                                            color: "#D6EBFF",
-                                                            borderBottomColor: "transparent",
-                                                        }}>
-                                                        <td style={{ borderTopLeftRadius: "6px", borderBottomLeftRadius: "6px" }}><center>{RestOfLeaderBoard.indexOf(item) + 4}</center></td>
+                                                    <tr key={item.userName} className="leaderboradRowStyle">
+                                                        <td className="leftCurvedBorders"><center>{RestOfLeaderBoard.indexOf(item) + 4}</center></td>
 
-                                                        <td key={item.picture} style={{
-                                                            backgroundColor: "#0080FF", WebkitBackgroundClip: "padding-box",
-                                                            backgroundClip: "content-box"
-
-                                                        }}><center>
-
-                                                                <Image src={item.picture} style={{
-                                                                    width: "2.2rem",
-                                                                    height: "2.2rem",
-                                                                    display: "block",
-                                                                    // objectFit: "contain",
-                                                                    // backgroundColor:"black",
-                                                                    verticalAlign: "middle",
-                                                                    borderRadius: "50%",
-                                                                    background: "red",
-                                                                    border: "2px solid #D6EBFF"
-                                                                }} alt="company logo"></Image>
-
-                                                            </center>
+                                                        <td key={item.picture}><center>
+                                                            <Image src={item.picture} className="leaderBoardAvatarStyle" alt="user avatar"></Image>
+                                                        </center>
                                                         </td>
                                                         <td><center>@{item.userName}</center></td>
                                                         <td><center>${item.amount.toFixed(2)}</center></td>
-                                                        <td style={{ borderTopRightRadius: "6px", borderBottomRightRadius: "6px" }}><center>+${item.dailyChange.toFixed(2)}</center></td>
+                                                        <td id={item.dailyChange} className="rightCurvedBorders"
+                                                            style={{ verticalAlign: "middle" }}>
+                                                            <center>+${item.dailyChange.toFixed(2)}<KeyboardDoubleArrowUpIcon style={{ fill: '#22ff00' }} /></center></td>
                                                     </tr>
-                                                    <tr key={`${item.userName}padding`} style={{ height: "5px", backgroundColor: "red" }} ></tr>
+                                                    <tr key={`${item.userName}padding`} style={{ height: "5px" }} ></tr>
                                                 </Fragment>)
 
                                             ))}
@@ -169,6 +147,3 @@ function LeaderBoard() {
 export default LeaderBoard;
 
 
-// fix image position for diff screen sizes
-// avatar images
-// 

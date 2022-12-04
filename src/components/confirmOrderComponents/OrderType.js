1af9074fd8f4ad3a4e9a08aaab57def7d42a8f8c
Warren <warrenkavanagh1000@gmail.com>
2022-12-04 21:14:51 +0000

    Added an info button for order type

--- src/components/confirmOrderComponents/OrderType.js
+++ src/components/confirmOrderComponents/OrderType.js
@@ -1,5 +1,6 @@
 import { Card, Button, Row, Col } from 'react-bootstrap';
 import { useState } from "react";
+import InfoButtonModal from '../widgets/InfoButtonModal/InfoButtonModal';
 
 function OrderType({orderType,setOrderType}) {
     const [active, setActive] = useState("");
@@ -16,7 +17,21 @@ function OrderType({orderType,setOrderType}) {
 
     return (
             <Card className="px-3">
-                    <h5 style={{ marginTop: "10px" }}>Order Type</h5>
+                    <h5 style={{ marginTop: "10px" }}>Order Type
+                     <InfoButtonModal title="Order Type" info={
+                        <>
+                        <p>There are two types of orders to choose from, these are <b>Market</b> and <b>Limit</b> orders.</p>
+                        <span className="semibolded">Market Orders - Instant</span>
+                        <p>Market Orders are orders that are completed instantly, if the user has enough money to do so.
+                        These are the most common types of orders that users will see in the real world.</p>
+                        <span className="semibolded">Limit Orders - Future</span>
+                        <p>Limit orders are orders that are put in place ahead of time based on the investor's predictions.
+                        For example, if an investor thinks that a stock may drop below a certain price, but that
+                        the stock is worth more than that price, they can place a limit order to buy that stock as soon as it
+                        hits that price. This is an example of an investor buying low in the hopes of making a profit when it increases.</p> 
+                        </>
+                   }/>
+                    </h5>
                     <Row style={{ display: "flex", alignItems: "center", justifyContent: "center", marginBottom: "10px" }}>
                         <Col style={{ display: "flex", alignItems: "center", justifyContent: "center", padding: "5px" }}>
                             <Button

d529d9145fe39b22e750711f3bda5956ce832641
Bearach Byrne <C15379616@mytudublin.ie>
2022-12-05 19:29:08 +0000

    Added icons and info buttons to categories

--- src/components/stockDiscoveryComponents/stockSummary/stockSummary.js
+++ src/components/stockDiscoveryComponents/stockSummary/stockSummary.js
@@ -6,7 +6,16 @@ import MessageAlert from '../../widgets/MessageAlert/MessageAlert';
 import SideScrollMenu from '../../widgets/SideScrollMenu/SideScrollMenu';
 import TickerCard from '../tickercard/Tickercard';
 import {useSelector} from 'react-redux';
+import InfoButtonModal from '../../widgets/InfoButtonModal/InfoButtonModal'
 
+import PersonIcon from '@mui/icons-material/Person';
+import TrendingUpIcon from '@mui/icons-material/TrendingUp';
+import TrendingDownIcon from '@mui/icons-material/TrendingDown';
+import EnergySavingsLeafIcon from '@mui/icons-material/EnergySavingsLeaf';
+import Groups3Icon from '@mui/icons-material/Groups3';
+import AssuredWorkloadIcon from '@mui/icons-material/AssuredWorkload';
+import LanIcon from '@mui/icons-material/Lan';
+import AttachMoneyIcon from '@mui/icons-material/AttachMoney';
 
 function StockSummary() {
     const [stocks, setStock] = useState({});
@@ -42,7 +51,12 @@ function StockSummary() {
         {loading ? <LoadingSpinner /> : error  ? <MessageAlert variant='danger'>{error}</MessageAlert> :
         <>
         
-        <h3 className="stockdiscoveryRow">Recommended For You</h3>
+        <h3 className="stockdiscoveryRow">Recommended For You <PersonIcon /><InfoButtonModal title="Personalised Recommendations" info={
+                        <div>
+                            <p>FinOptimise uses what's known as a content based recommender system to suggest companies that you may be interested in based on your most recent transaction. This Machine Learning model will find you the most similar companies from our database.</p> 
+                            <p> If you have not yet made a transaction, the recommender system will give you recommendations based on the highest positive mover over the past 24 hours.</p>
+                            <p>More infomation on content based recommender systems and how they work can be found <a href='https://towardsdatascience.com/introduction-to-recommender-systems-1-971bd274f421'>here.</a></p>
+                        </div>} /></h3>
         <SideScrollMenu>
                 {stocks[0].recommend.map((stockObj) => (
                         <div className='sideScrollCard' key={stockObj._id}>
@@ -51,7 +65,11 @@ function StockSummary() {
                      ))}
         </SideScrollMenu>
 
-        <h3 className="stockdiscoveryRow">Today's Biggest Positive Movers</h3>
+        <h3 className="stockdiscoveryRow">Top Gainers <TrendingUpIcon /><InfoButtonModal title="Top Gainers" info={
+                        <div>
+                            <p></p> 
+
+                        </div>} /></h3>
         <SideScrollMenu>
                 {stocks[0].topGainers.map((stockObj) => (
                         <div className='sideScrollCard' key={stockObj._id}>
@@ -61,7 +79,12 @@ function StockSummary() {
         </SideScrollMenu>
 
 
-        <h3 className="stockdiscoveryRow">Today's Biggest Negative Movers</h3>
+        <h3 className="stockdiscoveryRow">Top Losers <TrendingDownIcon /><InfoButtonModal title="Top Losers" info={
+                        <div>
+                            <p>FinOptimise uses what's known as a content based recommender system to suggest companies that you may be interested in based on your most recent transaction. This Machine Learning model will find you the most similar companies from our database.</p> 
+                            <p> If you have not yet made a transaction, the recommender system will give you recommendations based on the highest positive mover over the past 24 hours.</p>
+                            <p>More infomation on content based recommender systems and how they work can be found <a href='https://towardsdatascience.com/introduction-to-recommender-systems-1-971bd274f421'>here.</a></p>
+                        </div>} /></h3>
         <SideScrollMenu>
                 {stocks[0].topLosers.map((stockObj) => (
                         <div className='sideScrollCard' key={stockObj._id}>
@@ -71,7 +94,7 @@ function StockSummary() {
         </SideScrollMenu>
 
 
-        <h3 className="stockdiscoveryRow">Companies That Have Great Environmental Policies</h3>
+        <h3 className="stockdiscoveryRow">Top Environmental <EnergySavingsLeafIcon /></h3>
         <SideScrollMenu>
                 {stocks[0].topEnvironment.map((stockObj) => (
                         <div className='sideScrollCard' key={stockObj._id}>
@@ -81,7 +104,7 @@ function StockSummary() {
         </SideScrollMenu>
 
 
-        <h3 className="stockdiscoveryRow">Companies That Have Great Social Structures</h3>
+        <h3 className="stockdiscoveryRow">Companies That Have Great Social Structures <Groups3Icon /></h3>
         <SideScrollMenu>
                 {stocks[0].topSocial.map((stockObj) => (
                         <div className='sideScrollCard' key={stockObj._id}>
@@ -89,7 +112,7 @@ function StockSummary() {
                         </div>
                      ))}
         </SideScrollMenu>
-        <h3 className="stockdiscoveryRow">Companies That Have Great Governance</h3>
+        <h3 className="stockdiscoveryRow">Companies That Have Great Governance <AssuredWorkloadIcon /></h3>
         <SideScrollMenu>
                 {stocks[0].topGovernance.map((stockObj) => (
                         <div className='sideScrollCard' key={stockObj._id}>
@@ -97,7 +120,7 @@ function StockSummary() {
                         </div>
                      ))}
         </SideScrollMenu>
-        <h3 className="stockdiscoveryRow">Today's Top Moving Tech Stocks</h3>
+        <h3 className="stockdiscoveryRow">Today's Top Moving Tech Stocks <LanIcon /></h3>
         <SideScrollMenu>
                 {stocks[0].Technology.map((stockObj) => (
                         <div className='sideScrollCard' key={stockObj._id}>
@@ -105,7 +128,7 @@ function StockSummary() {
                         </div>
                      ))}
         </SideScrollMenu>
-        <h3 className="stockdiscoveryRow">Today's Top Moving Financial Service Stocks</h3>
+        <h3 className="stockdiscoveryRow">Today's Top Moving Financial Service Stocks <AttachMoneyIcon /></h3>
         <SideScrollMenu>
                 {stocks[0].Financial.map((stockObj) => (
                         <div className='sideScrollCard' key={stockObj._id}>

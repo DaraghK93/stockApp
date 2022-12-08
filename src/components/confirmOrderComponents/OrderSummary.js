217c69bc33d80ccc8bcb5e01c5ba043f3b6ea828
Warren <warrenkavanagh1000@gmail.com>
2022-12-08 12:20:30 +0000

    Added the accordion and removed a lot of the detail form the default view

--- src/components/confirmOrderComponents/OrderSummary.js
+++ src/components/confirmOrderComponents/OrderSummary.js
@@ -1,13 +1,49 @@
-import { Container, Table } from "react-bootstrap";
+import { Container, Table, Row } from "react-bootstrap";
+import Accordion from 'react-bootstrap/Accordion';
 
 function OrderSummary({ stockName, buyOrSell, orderType, dollarAmountSelected, newPortfolioBalance, qty, gameTradeFee}) {
 
     return (
-        <>
                 <Container>
                     <h5 style={{ marginTop: "10px" }}>Order Summary</h5>
-                    <Table>
-                        <tbody>
+                    <Row>
+                     <tbody>
+                            <tr>
+                                <td>Buy/ Sell</td>
+                                <td>{buyOrSell}</td>
+                            </tr>
+                            <tr>
+                                <td>Quantity</td>
+                                <td>{parseFloat(qty).toLocaleString('en-US', {minimumFractionDigits:0, maximumFractionDigits: 2})} stocks</td>
+                            </tr>
+                            {buyOrSell === "Buy" ?
+                                <tr>
+                                    <td className="bolded">Total</td>
+                                    <td className="bolded">{(parseFloat(dollarAmountSelected)+parseFloat(gameTradeFee)).toLocaleString('en-US', {style: 'currency', currency: 'USD' })}</td>
+                                </tr>
+                            :
+                            <>
+                            {parseFloat(dollarAmountSelected)-parseFloat(gameTradeFee) >= 0 ?
+                            <tr>
+                                <td className="bolded">Total</td>
+                                <td className="bolded">{(parseFloat(dollarAmountSelected)-parseFloat(gameTradeFee)).toLocaleString('en-US', {style: 'currency', currency: 'USD' })}</td>
+                            </tr>
+                            :
+                            <tr>
+                                <td className="bolded">Total</td>
+                                <td className="bolded">{(parseFloat(dollarAmountSelected)-parseFloat(gameTradeFee)).toLocaleString('en-US', {style: 'currency', currency: 'USD' })}</td>
+                            </tr>
+                            } 
+                            </>
+                        }
+                        </tbody>
+                    </Row>
+                    <Row className="py-5">
+                    <Accordion  defaultActiveKey="0">
+                        <Accordion.Item eventKey="0">
+                            <Accordion.Header align="center" style={{"justify-content": "center"}}>Trade Details</Accordion.Header>
+                            <Accordion.Body>
+                            <tbody>
                             <tr>
                                 <td>Stock</td>
                                 <td>{stockName}</td>
@@ -62,14 +98,12 @@ function OrderSummary({ stockName, buyOrSell, orderType, dollarAmountSelected, n
                                 <td className="bolded">{parseFloat(newPortfolioBalance).toLocaleString('en-US', {style: 'currency', currency: 'USD' })}</td>
                         </tr>
                         } 
-                            
-
-
-
                         </tbody>
-                    </Table>
+                            </Accordion.Body>
+                        </Accordion.Item>
+                    </Accordion>  
+                    </Row>
                 </Container>
-        </>
     )
 }
 

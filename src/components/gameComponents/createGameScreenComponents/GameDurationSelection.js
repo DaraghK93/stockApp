a7b30182b9cfb23609415b7904cf71070d4a5b83
Warren <warrenkavanagh1000@gmail.com>
2022-12-12 11:34:44 +0000

    Setting the error state when the game end date and start date is the same

--- src/components/gameComponents/createGameScreenComponents/GameDurationSelection.js
+++ src/components/gameComponents/createGameScreenComponents/GameDurationSelection.js
@@ -3,7 +3,7 @@ import Calendar from 'react-calendar';
 import 'react-calendar/dist/Calendar.css';
 import moment from 'moment';
 
-function GameDurationSelection({gameStartDate, setGameStartDate, gameEndDate, setGameEndDate, gameType}){
+function GameDurationSelection({gameStartDate, setGameStartDate, gameEndDate, setGameEndDate, gameType, gameDurationError, setGameDurationError}){
     const setDurationGame = (value,event) => {
         /// Used to set the duration game as package sets value as an array of dates 
         /// Set the start date 
@@ -11,6 +11,7 @@ function GameDurationSelection({gameStartDate, setGameStartDate, gameEndDate, se
         /// Set the end date 
         setGameEndDate(value[1])
         if (gameType === "timeBased" && value[0].getFullYear() === value[1].getFullYear() &&  value[0].getMonth() === value[1].getMonth() && value[0].getDate() === value[1].getDate()){
+            setGameDurationError("EEEOR")
             console.log("EQUAL")
             console.log("Start",value[0])
             console.log("END",value[1])

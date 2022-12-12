bb0b8dc44035f86655986300366885809022f883
Caolán Power <d21125982@mytudublin.ie>
2022-12-12 22:28:38 +0000

    daragh and becks centering change for all stocks

--- src/components/gameComponents/individualGameScreenComponents/GameStocksAll.js
+++ src/components/gameComponents/individualGameScreenComponents/GameStocksAll.js
@@ -78,7 +78,7 @@ function GameStocksAll({ league }) {
       ) : (
         <>
           <Container className='py-3'>
-            <Row>
+            <Row style={{ marginLeft:"auto", marginRight:"auto"}}>
               {stocks.map((stockObj) => (
                 <Col xs={6} md={3} xl={2} className='py-2' key={stockObj._id}>
                   <TickerCard

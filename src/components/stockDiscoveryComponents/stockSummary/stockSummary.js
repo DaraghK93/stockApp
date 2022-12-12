5a85a90fda15a4dd06a95945510f16e3cb2554fc
Bearach Byrne <C15379616@mytudublin.ie>
2022-12-12 17:26:05 +0000

    Changes class to className

--- src/components/stockDiscoveryComponents/stockSummary/stockSummary.js
+++ src/components/stockDiscoveryComponents/stockSummary/stockSummary.js
@@ -50,15 +50,15 @@ function StockSummary() {
         <h2 className="stockdiscoveryRowTitle">Suggested For You<Person/> <InfoButtonModalWhite title="Personalised Recommendations" info={
                         <div>
                             <p>FinOptimise uses a Machine Learning technology known as a content based recommender systems to suggest companies that you may be interested in. Our Machine Learning models provide two suggested rows:</p>
-                            <p><b>Recommended</b> provides recommendations of companies<mark class="positive">similar to your holdings.</mark></p>
-                            <p><b>Diversify Your Portfolio</b> provides recommendations of companies that are<mark class="negative">dissimilar to your holdings.</mark></p> 
+                            <p><b>Recommended</b> provides recommendations of companies<mark className="positive">similar to your holdings.</mark></p>
+                            <p><b>Diversify Your Portfolio</b> provides recommendations of companies that are<mark className="negative">dissimilar to your holdings.</mark></p> 
                             <p>More infomation on content based recommender systems and how they work can be found <a href='https://towardsdatascience.com/introduction-to-recommender-systems-1-971bd274f421'>here.</a></p>
                         </div>} /></h2>
         <h3 className="stockdiscoveryRow">Recommended <IcBaselineCurrencyExchange/> <InfoButtonModalWhite title="Recommended" info={
                         <div>
                             
-                            <p>This Machine Learning model will suggest companies from our database that are<mark class="positive">similar</mark>to your most recent transaction.</p>
-                            <p> If you have not yet made a transaction, the recommender system will give you recommendations based on companies<mark class="positive">similar</mark>to the<mark class="positive">highest positive mover</mark>over the<mark class="bold">past 24 hours.</mark></p>
+                            <p>This Machine Learning model will suggest companies from our database that are<mark className="positive">similar</mark>to your most recent transaction.</p>
+                            <p> If you have not yet made a transaction, the recommender system will give you recommendations based on companies<mark className="positive">similar</mark>to the<mark className="positive">highest positive mover</mark>over the<mark className="bold">past 24 hours.</mark></p>
                         </div>} /></h3>
         <SideScrollMenu>
                 {stocks[0].recommend.map((stockObj) => (
@@ -69,8 +69,8 @@ function StockSummary() {
         </SideScrollMenu>
         <h3 className="stockdiscoveryRow">Diversify Your Portfolio <Fa6SolidChartPie/> <InfoButtonModalWhite title="Diversify Your Portfolio" info={
                         <div>
-                            <p>This Machine Learning model will suggest companies from our database that are<mark class="negative">dissimilar</mark>to your most recent transaction.</p>
-                            <p> If you have not yet made a transaction, the recommender system will give you recommendations based on companies<mark class="negative">dissimilar</mark>to the<mark class="positive">highest positive mover</mark>over the<mark class="bold">past 24 hours.</mark></p>
+                            <p>This Machine Learning model will suggest companies from our database that are<mark className="negative">dissimilar</mark>to your most recent transaction.</p>
+                            <p> If you have not yet made a transaction, the recommender system will give you recommendations based on companies<mark className="negative">dissimilar</mark>to the<mark className="positive">highest positive mover</mark>over the<mark className="bold">past 24 hours.</mark></p>
                         </div>} /></h3>
         <SideScrollMenu>
                 {stocks[0].derecommend.map((stockObj) => (
@@ -82,11 +82,11 @@ function StockSummary() {
 
         <h2 className="stockdiscoveryRowTitle">Top Movers <Timeline /> <InfoButtonModalWhite title="Top Movers" info={
                         <div>
-                            <p>This section displays the top<mark class="positive">positive</mark>and<mark class="negative">negative</mark> movers over the<mark class="bold">past 24 hour period.</mark></p>
+                            <p>This section displays the top<mark className="positive">positive</mark>and<mark className="negative">negative</mark> movers over the<mark className="bold">past 24 hour period.</mark></p>
                         </div>} /></h2>
         <h3 className="stockdiscoveryRow">Top Gainers <TrendingUp /><InfoButtonModal title="Top Gainers" info={
                         <div>
-                            <p>This section shows the<mark class="bold">top 20 companies</mark>ranked by<mark class="positive">largest positive % change</mark>in stock price over the<mark class="bold">past 24 hour period.</mark></p> 
+                            <p>This section shows the<mark className="bold">top 20 companies</mark>ranked by<mark className="positive">largest positive % change</mark>in stock price over the<mark className="bold">past 24 hour period.</mark></p> 
                             <p>An explanation for how price change is calculated is explained <a href='https://www.investopedia.com/terms/c/change.asp'>here.</a></p> 
                         </div>} /></h3>
         <SideScrollMenu>
@@ -99,7 +99,7 @@ function StockSummary() {
 
                     <h3 className="stockdiscoveryRow">Top Losers <TrendingDown /><InfoButtonModal title="Top Losers" info={
                         <div>
-                            <p>This section shows the<mark class="bold">top 20 companies</mark>ranked by<mark class="negative">largest negative % change</mark>in stock price over the<mark class="bold">past 24 hour period.</mark></p>
+                            <p>This section shows the<mark className="bold">top 20 companies</mark>ranked by<mark className="negative">largest negative % change</mark>in stock price over the<mark className="bold">past 24 hour period.</mark></p>
                             <p>An explanation for how price change is calculated is explained <a href='https://www.investopedia.com/terms/c/change.asp'>here.</a></p>
                         </div>} /></h3>
         <SideScrollMenu>
@@ -112,12 +112,12 @@ function StockSummary() {
 
         <h2 className="stockdiscoveryRowTitle">Top ESG Stocks <Forest /> <InfoButtonModalWhite title="Top ESG" info={
                         <div>
-                            <p>This section shows the<mark class="bold">top 20 companies</mark> ranked by<mark class="positive">Environmental, Social and Governance (ESG) ratings.</mark></p>
+                            <p>This section shows the<mark className="bold">top 20 companies</mark> ranked by<mark className="positive">Environmental, Social and Governance (ESG) ratings.</mark></p>
                             <p>An explanation for ESG ratings, along with how they're calculated can be found <Link to="/faqs">here</Link> on our education page. Just scroll down to the "What do I need to know about ESG?" section.</p>
                         </div>} /></h2>
                         <h3 className="stockdiscoveryRow">Top Environmental <EnergySavingsLeaf /><InfoButtonModal title="Top Environmental" info={
                             <div>
-                                <p>This section shows the<mark class="bold">top 20 companies</mark>ranked by their<mark class="positive">Environmental rating</mark>, as taken from their ESG scores.</p>
+                                <p>This section shows the<mark className="bold">top 20 companies</mark>ranked by their<mark className="positive">Environmental rating</mark>, as taken from their ESG scores.</p>
                                 <p>An explanation for Environmental ratings, along with how they're calculated can be found <Link to="/faqs">here</Link> on our education page. Just scroll down to the "What do I need to know about ESG?" section.</p>
                             </div>} /></h3>
                         <SideScrollMenu>
@@ -129,7 +129,7 @@ function StockSummary() {
                         </SideScrollMenu>
                         <h3 className="stockdiscoveryRow">Top Social <Groups3 /><InfoButtonModal title="Top Social" info={
                             <div>
-                                <p>This section shows the<mark class="bold">top 20 companies</mark>ranked by their<mark class="positive">Social rating</mark>, as taken from their ESG scores.</p>
+                                <p>This section shows the<mark className="bold">top 20 companies</mark>ranked by their<mark className="positive">Social rating</mark>, as taken from their ESG scores.</p>
                                 <p>An explanation for Social ratings, along with how they're calculated can be found <Link to="/faqs">here</Link> on our education page. Just scroll down to the "What do I need to know about ESG?" section.</p>
                             </div>} /></h3>
                         <SideScrollMenu>
@@ -141,7 +141,7 @@ function StockSummary() {
                         </SideScrollMenu>
                         <h3 className="stockdiscoveryRow">Top Governance <AssuredWorkload /><InfoButtonModal title="Top Governance" info={
                             <div>
-                                <p>This section shows the<mark class="bold">top 20 companies</mark>ranked by their<mark class="positive">Governance rating</mark>, as taken from their ESG scores.</p>
+                                <p>This section shows the<mark className="bold">top 20 companies</mark>ranked by their<mark className="positive">Governance rating</mark>, as taken from their ESG scores.</p>
                                 <p>An explanation for Governance ratings, along with how they're calculated can be found <Link to="/faqs">here</Link> on our education page. Just scroll down to the "What do I need to know about ESG?" section.</p>
                             </div>} /></h3>
                         <SideScrollMenu>
@@ -153,7 +153,7 @@ function StockSummary() {
                         </SideScrollMenu>
         <h2 className="stockdiscoveryRowTitle">Top Stocks By Sector <DataUsage /> <InfoButtonModalWhite title="Top Stocks By Sector" info={
                         <div>
-                            <p>This section shows the<mark class="bold">top 20 companies</mark>ranked by<mark class="positive">largest positive % change</mark>in stock price over the<mark class="bold">past 24 hour period</mark>in each of the 11 sectors in the S&P500.</p>
+                            <p>This section shows the<mark className="bold">top 20 companies</mark>ranked by<mark className="positive">largest positive % change</mark>in stock price over the<mark className="bold">past 24 hour period</mark>in each of the 11 sectors in the S&P500.</p>
                             <p>Detailed explanations of these 11 sectors can be found <a href='https://corporatefinanceinstitute.com/resources/valuation/the-sp-sectors/'>here.</a></p>
                         </div>} /></h2>
         <h3 className="stockdiscoveryRow">Industrials <Factory /></h3>

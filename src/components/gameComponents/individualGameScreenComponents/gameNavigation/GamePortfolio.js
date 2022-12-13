dc5fd8deff841dd48a449b0768ad61fb4f4882d0
Rebecca Kelly <bex.r.kelly@gmail.com>
2022-12-13 15:44:55 +0000

    formatting for porfolio done

--- src/components/gameComponents/individualGameScreenComponents/gameNavigation/GamePortfolio.js
+++ src/components/gameComponents/individualGameScreenComponents/gameNavigation/GamePortfolio.js
@@ -51,7 +51,7 @@ function GamePortfolio({ data, name, totalValue }) {
                                     <dl className='infoList' style={{ padding: 0 }}>
                                         <dt style={{ color: redOrGreen() }}> {positiveSymbol}{parseFloat(absoluteChange).toLocaleString('en-US', { style: 'currency', currency: 'USD' })} ({positiveSymbol}{percentageChange.toFixed(2)}%)</dt>
                                     </dl>
-                                    <StockPriceChart data={data} lineColor={lineColor} gradientColor={gradientColor} dataKey={"value"} datetype={"monthly"} />
+                                    <StockPriceChart data={data} lineColor={lineColor} gradientColor={gradientColor} dataKey={"value"} datetype={"port"} />
                                 </>
                             }
                         </Col>
